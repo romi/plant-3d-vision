9570c6235ca4ddb41cb06725872605ee68ebf2e4
Timothée Wintz <timothee@timwin.fr>
2019-02-05 12:00:16 +0100

    masking new interface

--- lettucescan/pipeline/masking.py
+++ lettucescan/pipeline/masking.py
@@ -16,28 +16,55 @@ def excess_green(x):
 
 
 class Masking(ProcessingBlock):
+    def read_input(self, scan, endpoint):
+        fileset = scan.get_fileset(endpoint)
+
+        scanner_metadata = scan.get_metadata('scanner')
+        self.camera = scanner_metadata['camera_model']
+
+        self.images = []
+        for f in fileset.get_files():
+            data = f.read_image()
+            self.images.append({
+                'id': f.id,
+                'data': data,
+                'metadata': f.get_metadata()
+            })
+
+    def write_output(self, scan, endpoint):
+        fileset = scan.get_fileset(endpoint)
+        for img in self.masks:
+            f = fileset.get_file(img['id'])
+            if f is None:
+                f = fileset.create_file(img['id'])
+                f.write_image('png', img['data'])
+                f.set_metadata(img['metadata'])
+
+
     def __init__(self, input_fileset, output_fileset, f):
         self.f = f
-        self.input_fileset = input_fileset
-        self.output_fileset = output_fileset
 
     def process(self):
-        for i, file in enumerate(self.input_fileset.get_files()):
-            im = file.read_image()
+        self.masks = []
+        for img in self.images:
+            im = img['data']
             im = np.asarray(im, dtype=float) / 255.0
-            mask = np.asarray((self.f(im) * 255), dtype=np.uint8)
-            mask_file = self.output_fileset.create_file(file.id)
-            mask_file.write_image('png', mask)
+            mask_data = np.asarray((self.f(im) * 255), dtype=np.uint8)
+            self.undistorted_images.append({
+                'id': img['id'],
+                'data': mask_data,
+                'metadata': img['metadata']
+            })
 
 
 class ExcessGreenMasking(Masking):
-    def __init__(self, input_filesets, output_filesets, threshold):
+    def __init__(self, threshold):
         def f(x): return excess_green(x) > threshold
-        super().__init__(input_filesets, output_filesets, f)
+        super().__init__(f)
 
 
 class LinearMasking(Masking):
-    def __init__(self, input_filesets, output_filesets, coefs):
+    def __init__(self, coefs):
         def f(x): return (coefs[0] * x[:, :, 0] + coefs[1] * x[:, :, 1] +
                           coefs[2] * x[:, :, 2]) > coefs[3]
-        super().__init__(input_filesets, output_filesets, f)
+        super().__init__(f)
--- tests/test_masking.py
+++ tests/test_masking.py
@@ -2,25 +2,16 @@
 import sys
 sys.path.append("..")
 
-from lettucescan import masking
+from lettucescan.pipeline import masking
 import lettucethink.fsdb as db
 
 
 datab = db.DB("../test-db/")
 
 scan = datab.get_scan("2019-01-22_15-57-34")
-fileset_images = scan.get_fileset("images")
 
-fileset_masks_1= scan.get_fileset("masks_1")
-if fileset_masks_1 is None:
-    fileset_masks_1 = scan.create_fileset("masks_1")
-
-fileset_masks_2 = scan.get_fileset("masks_2")
-if fileset_masks_2  is None:
-    fileset_masks_2  = scan.create_fileset("masks_2")
-
-params_1 = masking.ExcessGreenMasking(0)
-params_2 = masking.LinearMasking([0.0, 1.0, 0.0, 0.3])
+masking_1 = masking.ExcessGreenMasking(0)
+masking_2 = masking.LinearMasking([0.0, 1.0, 0.0, 0.3])
 
 input_filesets = {
     'images' : fileset_images
