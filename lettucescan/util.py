67b5614f16c0e2c30f2d87cb342962fe612d2c69
Timothée Wintz <timothee@timwin.fr>
2019-02-05 14:04:51 +0100

    colmap write output

--- lettucescan/util.py
+++ lettucescan/util.py
@@ -7,17 +7,17 @@ def db_read_point_cloud(file):
     return open3d.read_point_cloud(path)
 
 def db_write_point_cloud(file, pcd):
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        pcd_path = os.path.join(tmpdirname.name, 'pcd.ply')
-        open3d.write_point_cloud(pcd_path, pcd)
-        file.import_file(pcd_path)
+    tmpdir = tempfile.TemporaryDirectory()
+    pcd_path = os.path.join(tmpdir.name, 'pcd.ply')
+    open3d.write_point_cloud(pcd_path, pcd)
+    file.import_file(pcd_path)
 
 def db_read_triangle_mesh(file):
     path = fsdb._file_path(file, file.filename)
     return open3d.read_triangle_mesh(path)
 
 def db_write_triangle_mesh(file, mesh):
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        mesh_path = os.path.join(tmpdirname.name, 'mesh.ply')
+        tmpdir = tempfile.TemporaryDirectory()
+        mesh_path = os.path.join(tmpdir.name, 'mesh.ply')
         open3d.write_triangle_mesh(mesh_path, mesh)
         file.import_file(mesh_path)
