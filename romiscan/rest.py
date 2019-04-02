0d81246ef4598b718d2976a510806dd56c2bf84f
Timothée Wintz <timothee@timwin.fr>
2019-04-02 16:00:19 +0200

    viz

--- romiscan/rest.py
+++ romiscan/rest.py
@@ -166,4 +166,4 @@ api.add_resource(Scan, '/scans/<scan_id>')
 api.add_resource(File, '/files/<path:path>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
