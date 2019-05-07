12911dbefd128345e0d03ad5722d02519773c53f
Timothée Wintz <timothee@timwin.fr>
2019-05-07 11:57:38 +0200

    install open3d fix

--- utils/install_open3d.py
+++ utils/install_open3d.py
@@ -3,20 +3,25 @@ import tempfile
 import os
 import subprocess
 from shutil import copyfile
-from distutils.sysconfig import get_python_lib
+from distutils.sysconfig import get_python_lib, get_python_inc
 import site
 import glob
+import sysconfig
+import sys
 
 cur_dir = os.getcwd()
 try:
     with tempfile.TemporaryDirectory() as tmpdir:
+        python_lib_so = os.path.join(sysconfig.get_config_var('LIBDIR'), sysconfig.get_config_var('PY3LIBRARY'))
+        python_include = sysconfig.get_config_var('INCLUDEDIR')
+        python_exec = sys.executable
         os.chdir(tmpdir)
         subprocess.check_call(['git', 'clone', 'https://github.com/IntelVCL/Open3D'])
         os.chdir("Open3D")
         subprocess.check_call(['git', 'submodule', 'update',  '--init', '--recursive'])
         os.makedirs('build')
         os.chdir('build')
-        subprocess.check_call(['cmake', '..'])
+        subprocess.check_call(['cmake', '..', '-DPYTHON_EXECUTABLE=%s'%python_exec, '-DPYTHON_LIBRARY=%s'%python_lib_so, '-DPYTHON_INCLUDE=%s'%python_include])
         subprocess.check_call(['make'])
         python_lib_path = get_python_lib()
         for x in glob.glob('lib/Python/*.so'):
