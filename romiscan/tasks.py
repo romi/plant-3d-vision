1d73727ac380911aa9dd64d37eb6cdc74dd52d0b
Timothée Wintz <timothee@timwin.fr>
2019-07-30 11:28:24 +0200

    visualization for 0.5

--- romiscan/tasks.py
+++ romiscan/tasks.py
@@ -15,6 +15,10 @@ import json
 import numpy as np
 import importlib
 import os
+import open3d
+import tempfile
+import shutil
+from skimage.transform import resize
 
 from romidata.task import ImagesFilesetExists, RomiTask, FileByFileTask
 from romidata import io
@@ -305,3 +309,90 @@ class AnglesAndInternodes(RomiTask):
         t = io.read_graph(self.input_file())
         measures = arabidopsis.compute_angles_and_internodes(t)
         io.write_json(self.output_file(), measures)
+
+class Visualization(RomiTask):
+    """Prepares files for visualization
+    """
+    upstream_task = None
+
+    upstream_point_cloud = luigi.TaskParameter(default=PointCloud)
+    upstream_mesh = luigi.TaskParameter(default=TriangleMesh)
+    upstream_colmap = luigi.TaskParameter(default=Colmap)
+    upstream_angles = luigi.TaskParameter(default=AnglesAndInternodes)
+    upstream_skeleton = luigi.TaskParameter(default=CurveSkeleton)
+    upstream_images = luigi.TaskParameter(default=Undistorted)
+
+    max_image_size = luigi.IntParameter()
+    max_point_cloud_size = luigi.IntParameter()
+    thumbnail_size = luigi.IntParameter()
+
+    def requires(self):
+        return []
+
+    def run(self):
+        def resize_to_max(img, max_size):
+            i = np.argmax(img.shape[0:2])
+            if img.shape[i] <= max_size:
+                return img
+            if i == 0:
+                new_shape = [max_size, int(max_size * img.shape[1]/img.shape[0])]
+            else:
+                new_shape = [int(max_size * img.shape[0]/img.shape[1]), max_size]
+            return resize(img, new_shape)
+
+        output_fileset = self.output().get()
+
+        # ZIP
+        scan = self.output().scan
+        basedir = scan.db.basedir
+        print("basedir = %s"%basedir)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            shutil.make_archive(os.path.join(tmpdir, "scan"), "zip",
+                                os.path.join(basedir, scan.id))
+            f = output_fileset.get_file('scan', create=True)
+            f.import_file(os.path.join(tmpdir, 'scan.zip'))
+
+        # ANGLES
+        if self.upstream_angles().complete():
+            angles_file = self.upstream_angles().output_file()
+            f = output_fileset.create_file(angles_file.id)
+            io.write_json(f, io.read_json(angles_file))
+
+        # SKELETON
+        if self.upstream_skeleton().complete():
+            skeleton_file = self.upstream_skeleton().output_file()
+            f = output_fileset.create_file(skeleton_file.id)
+            io.write_json(f, io.read_json(skeleton_file))
+
+        # MESH
+        if self.upstream_mesh().complete():
+            mesh_file = self.upstream_mesh().output_file()
+            f = output_fileset.create_file(mesh_file.id)
+            io.write_triangle_mesh(f, io.read_triangle_mesh(mesh_file))
+
+        # PCD
+        if self.upstream_point_cloud().complete():
+            point_cloud_file = self.upstream_point_cloud().output_file()
+            point_cloud = io.read_point_cloud(point_cloud_file)
+            f = output_fileset.create_file(point_cloud_file.id)
+            if len(point_cloud.points) < self.max_point_cloud_size:
+                point_cloud_lowres = point_cloud
+            else:
+                point_cloud_lowres = open3d.geometry.uniform_down_sample(point_cloud, len(point_cloud.points) // self.max_point_cloud_size + 1)
+            io.write_point_cloud(f, point_cloud)
+
+        # IMAGES
+        images_fileset = self.upstream_images().output().get()
+        for img in images_fileset.get_files():
+            data = io.read_image(img)
+            # remove alpha channel
+            if data.shape[2] == 4:
+                data = data[:,:,:3]
+            lowres = resize_to_max(data, self.max_image_size)
+            thumbnail = resize_to_max(data, self.thumbnail_size)
+
+            f = output_fileset.create_file("lowres_%s"%img.id)
+            io.write_image(f, lowres)
+            f = output_fileset.create_file("thumbnail_%s"%img.id)
+            io.write_image(f, thumbnail)
+
