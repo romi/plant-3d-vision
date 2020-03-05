0bfcbb15476d675ff33a84a792fe3e6e433ba230
Timothée Wintz <timothee@timwin.fr>
2020-03-05 11:02:54 +0100

    add config

---
+++ romiscan/tasks/config.py
@@ -0,0 +1,11 @@
+import luigi
+
+class PointCloudColorConfig(luigi.Config):
+    colors = luigi.DictParameter(default = {
+        "stem" : [1.0, 0.0, 0.0],
+        "flower" : [1.0, 1.0, 0.0],
+        "fruit" : [1.0, 0.0, 1.0],
+        "pedicel" : [1.0, 1.0, 1.0],
+        "leaf" : [0.0, 1.0, 0.0],
+    })
+    
