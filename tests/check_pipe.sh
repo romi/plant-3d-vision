ee4177261643009ba7f4d6d8b000e2ed9923a13f
naittaleb <aittalebnabil@gmail.com>
2020-04-23 01:13:17 +0200

    Clean before tests and download appropriate model

--- tests/check_pipe.sh
+++ tests/check_pipe.sh
@@ -1,13 +1,34 @@
+#!/bin/bash
+
 ##### Check geometric pipeline
-romi_run_task --config ../config/original_pipe_0.toml AnglesAndInternodes real_plant/ --local-scheduler
+# 1. clean
+romi_run_task --config ../config/original_pipe_0.toml Clean testdata/real_plant/ --local-scheduler
+# 2. run pipeline
+romi_run_task --config ../config/original_pipe_0.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
 
 ##### Check machine learning pipeline
 # 1. download models
-wget https://media.romi-project.eu/data/Resnetdataset_gl_png_896_896_epoch50.pt
-cp Resnetdataset_gl_png_896_896_epoch50.pt models/models/
+MODEL_DIRECTORY="testdata/models/models"
 
-wget https://media.romi-project.eu/data/tmp_epoch40.pt
-cp tmp_epoch40.pt models/models/
+if [ ! -d ${MODEL_DIRECTORY} ]; then
+	mkdir ${MODEL_DIRECTORY}
+fi
 
-# 2. run pipeline
-romi_run_task --config ../config/ml_pipe_vplants_3.toml PointCloud virtual_plant/ --local-scheduler
+MODEL_EPOCH_896_896_50="${MODEL_DIRECTORY}/Resnetdataset_gl_png_896_896_epoch50.pt"
+
+if [ ! -f ${MODEL_EPOCH_896_896_50} ]; then
+	wget https://media.romi-project.eu/data/Resnetdataset_gl_png_896_896_epoch50.pt
+	mv Resnetdataset_gl_png_896_896_epoch50.pt ${MODEL_DIRECTORY}
+fi
+
+MODEL_TMP_40="${MODEL_DIRECTORY}/tmp_epoch40.pt"
+if [ ! -f ${MODEL_TMP_40} ]; then
+	wget https://media.romi-project.eu/data/tmp_epoch40.pt
+	mv tmp_epoch40.pt ${MODEL_DIRECTORY}
+fi
+
+# 2. clean
+romi_run_task --config ../config/ml_pipe_vplants_3.toml Clean testdata/virtual_plant/ --local-scheduler
+
+# 3. run pipeline
+romi_run_task --config ../config/ml_pipe_vplants_3.toml PointCloud testdata/virtual_plant/ --local-scheduler
