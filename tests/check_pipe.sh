a7aadf9679de869ef964c61d910d7e6c854c9f85
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2020-08-19 17:53:36 +0200

    Add call to print_task_info for latest task 'AnglesAndInternodes'
    Commented out the download of 'tmp_epoch40.pt' model.
    It is not used by configuration file in config/ml_pipe_vplants_4.toml

--- tests/check_pipe.sh
+++ tests/check_pipe.sh
@@ -5,6 +5,7 @@
 romi_run_task --config ../config/original_pipe_1.toml Clean testdata/real_plant/ --local-scheduler
 # 2. run pipeline
 romi_run_task --config ../config/original_pipe_1.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
+print_task_info AnglesAndInternodes testdata/real_plant/
 
 ##### Check machine learning pipeline
 # 1. download models
@@ -21,14 +22,14 @@ if [ ! -f ${MODEL_EPOCH_896_896_50} ]; then
 	mv Resnetdataset_gl_png_896_896_epoch50.pt ${MODEL_DIRECTORY}
 fi
 
-MODEL_TMP_40="${MODEL_DIRECTORY}/tmp_epoch40.pt"
-if [ ! -f ${MODEL_TMP_40} ]; then
-	wget https://media.romi-project.eu/data/tmp_epoch40.pt
-	mv tmp_epoch40.pt ${MODEL_DIRECTORY}
-fi
+#MODEL_TMP_40="${MODEL_DIRECTORY}/tmp_epoch40.pt"
+#if [ ! -f ${MODEL_TMP_40} ]; then
+#	wget https://media.romi-project.eu/data/tmp_epoch40.pt
+#	mv tmp_epoch40.pt ${MODEL_DIRECTORY}
+#fi
 
 # 2. clean
 romi_run_task --config ../config/ml_pipe_vplants_4.toml Clean testdata/virtual_plant/ --local-scheduler
-
 # 3. run pipeline
 romi_run_task --config ../config/ml_pipe_vplants_4.toml PointCloud testdata/virtual_plant/ --local-scheduler
+print_task_info AnglesAndInternodes testdata/virtual_plant/
