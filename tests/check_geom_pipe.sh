8787c9ab782b21040134653e963bbf8148878747
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2020-09-03 18:28:25 +0200

    Add Colmap info

--- tests/check_geom_pipe.sh
+++ tests/check_geom_pipe.sh
@@ -5,5 +5,11 @@
 romi_run_task --config ../config/original_pipe_1.toml Clean testdata/real_plant/ --local-scheduler
 # 2. run pipeline
 romi_run_task --config ../config/original_pipe_1.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
-print_task_info PointCloud testdata/real_plant/
+
+echo ""
+print_task_info Colmap testdata/real_plant/
+echo "
+"print_task_info PointCloud testdata/real_plant/
+echo "
+"
 print_task_info AnglesAndInternodes testdata/real_plant/
