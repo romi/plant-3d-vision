4dcf7566d7c9828160d001cdad1efed51ab0f2b9
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2020-09-03 17:39:36 +0200

    Add build & run Dockerfile scripts & improve Dockerfile

---
+++ docker/build.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+###############################################################################
+# Example usages:
+###############################################################################
+# 1. Default build options:
+# $ ./build.sh
+#
+# 2. Build image with 'debug' image tag & another 'romiscan' branch options:
+# $ ./build.sh -t debug -b 'feature/faster_docker'
+
+user=$USER
+uid=$(id -u)
+gid=$(id -g)
+vtag="latest"
+romidata_branch='dev'
+romiscan_branch='dev'
+romiscanner_branch='master'
+
+usage() {
+  echo "USAGE:"
+  echo "  ./build.sh [OPTIONS]
+    "
+
+  echo "DESCRIPTION:"
+  echo "  Build a docker image named 'roboticsmicrofarms/romiscan' using Dockerfile in same location.
+    "
+
+  echo "OPTIONS:"
+  echo "  -t, --tag
+    Docker image tag to use, default to '$vtag'.
+    "
+  echo "  -u, --user
+    User name to create inside docker image, default to '$user'.
+    "
+  echo "  --uid
+    User id to use with 'user' inside docker image, default to '$uid'.
+    "
+  echo "  --gid
+    Group id to use with 'user' inside docker image, default to '$gid'.
+    "
+  echo "  -b, --romiscan
+    Git branch to use for cloning 'romiscan' inside docker image, default to '$romiscan_branch'.
+    "
+  echo "  --romidata
+    Git branch to use for cloning 'romidata' inside docker image, default to '$romidata_branch'.
+    "
+  echo "  --romiscanner
+    Git branch to use for cloning 'romiscanner' inside docker image, default to '$romiscanner_branch'.
+    "
+  echo "  -h, --help
+    Output a usage message and exit.
+    "
+}
+
+while [ "$1" != "" ]; do
+  case $1 in
+  -t | --tag)
+    shift
+    vtag=$1
+    ;;
+  -u | --user)
+    shift
+    user=$1
+    ;;
+  --uid)
+    shift
+    user=$1
+    ;;
+  --gid)
+    shift
+    user=$1
+    ;;
+  -b | --romiscan)
+    shift
+    romiscan_branch=$1
+    ;;
+  --romidata)
+    shift
+    romidata_branch=$1
+    ;;
+  --romiscanner)
+    shift
+    romiscanner_branch=$1
+    ;;
+  -h | --help)
+    usage
+    exit
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+docker build -t roboticsmicrofarms/romiscan:$vtag \
+  --build-arg USER_NAME=$user --build-arg USER_ID=$uid --build-arg GROUP_ID=$gid \
+  --build-arg ROMISCAN_BRANCH=$romiscan_branch --build-arg ROMIDATA_BRANCH=$romidata_branch --build-arg ROMISCANNER_BRANCH=$romiscanner_branch \
+  .
---
+++ docker/run.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+###############################################################################
+# Example usages:
+###############################################################################
+# 1. Default run starts an interactive shell:
+# $ ./run.sh
+#
+# 2. Run container using 'debug' image tag & start GPU test:
+# $ ./run.sh --gpu_test
+#
+# 3. Run container using 'debug' image tag & start CI tests:
+# $ ./run.sh --pipeline_test
+#
+# 4. Run container using 'debug' image tag & start a ROMI Task:
+# $ ./run.sh -t debug -p "/data/ROMI/DB" -c "romi_run_task AnglesAndInternodes ~/db/2019-02-01_10-56-33 --config ~/romiscan/config/original_pipe_0.toml"
+
+user=$USER
+vtag="latest"
+cmd=''
+pipeline_cmd="cd romiscan/tests/ && ./check_pipe.sh"
+geom_pipeline_cmd="cd romiscan/tests/ && ./check_geom_pipe.sh"
+ml_pipeline_cmd="cd romiscan/tests/ && ./check_ml_pipe.sh"
+gpu_cmd="nvidia-smi"
+
+mount_option=''
+
+usage() {
+  echo "USAGE:"
+  echo "  ./run.sh [OPTIONS]
+    "
+
+  echo "DESCRIPTION:"
+  echo "  Run 'roboticsmicrofarms/romiscan:<vtag>' container with a mounted local (host) database.
+    "
+
+  echo "OPTIONS:"
+  echo "  -t, --tag
+    Docker image tag to use, default to '$vtag'.
+    "
+  echo "  -p, --database_path
+    Path to the host database to mount inside docker container.
+    "
+  echo "  -u, --user
+    User used during docker image build, default to '$user'.
+    "
+
+  echo "  -c, --cmd
+    Defines the command to run at docker startup, by default start an interactive container with a bash shell.
+    "
+  echo "  --pipeline_test
+    Test pipelines (geometric & ML based) in docker container with CI test.
+    "
+  echo "  --geom_pipeline_test
+    Test geometric pipeline in docker container with CI test & test dataset.
+    "
+  echo "  --ml_pipeline_test
+    Test ML pipeline in docker container with CI test & test dataset.
+    "
+  echo "  --gpu_test
+    Test correct access to NVIDIA GPU resources from docker container.
+    "
+
+  echo "  -h, --help
+    Output a usage message and exit.
+    "
+}
+
+while [ "$1" != "" ]; do
+  case $1 in
+  -t | --tag)
+    shift
+    vtag=$1
+    ;;
+  -u | --user)
+    shift
+    user=$1
+    ;;
+  -p | --database_path)
+    shift
+    mount_option="-v $1:/home/$user/db"
+    ;;
+  -c | --cmd)
+    shift
+    cmd=$1
+    ;;
+  --pipeline_test)
+    cmd=$pipeline_cmd
+    ;;
+  --geom_pipeline_test)
+    cmd=$geom_pipeline_cmd
+    ;;
+  --ml_pipeline_test)
+    cmd=$ml_pipeline_cmd
+    ;;
+  --gpu_test)
+    cmd=$gpu_cmd
+    ;;
+  -h | --help)
+    usage
+    exit
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+if [ "$cmd" = "" ]
+then
+  # Start in interactive mode:
+  docker run --runtime=nvidia --gpus all $mount_option \
+    --env PYOPENCL_CTX='0' \
+    -it roboticsmicrofarms/romiscan:$vtag
+else
+  # Start in non-interactive mode (run the command):
+  docker run --runtime=nvidia --gpus all $mount_option \
+    --env PYOPENCL_CTX='0' \
+    roboticsmicrofarms/romiscan:$vtag \
+    bash -c "$cmd"
+fi
