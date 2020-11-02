9e1d1098ee2c64e9fbeee92cdfb976a1faece27c
Jonathan LEGRAND <jlegrand62@gmail.com>
2020-11-02 18:20:54 +0100

    Add ML pipeline on real_plant dataset

--- tests/check_ml_pipe.sh
+++ tests/check_ml_pipe.sh
@@ -22,8 +22,15 @@ fi
 #fi
 
 # 2. clean
-romi_run_task --config ../config/test_ml_pipe.toml Clean testdata/virtual_plant/ --local-scheduler
+romi_run_task --config ../config/test_ml_pipe.toml Clean testdata/real_plant/ --local-scheduler
 # 3. run pipeline
-romi_run_task --config ../config/test_ml_pipe.toml AnglesAndInternodes testdata/virtual_plant/ --local-scheduler
+romi_run_task --config ../config/test_ml_pipe.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
+print_task_info PointCloud testdata/real_plant/
+print_task_info AnglesAndInternodes testdata/real_plant/
+
+# 4. clean
+romi_run_task --config ../config/ml_pipe_vplants_3.toml Clean testdata/virtual_plant/ --local-scheduler
+# 5. run pipeline
+romi_run_task --config ../config/ml_pipe_vplants_3.toml AnglesAndInternodes testdata/virtual_plant/ --local-scheduler
 print_task_info PointCloud testdata/virtual_plant/
 print_task_info AnglesAndInternodes testdata/virtual_plant/
