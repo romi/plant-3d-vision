26a6d8fd5ec6a838df63e9b543337565449d7753
Jonathan LEGRAND <jlegrand62@gmail.com>
2020-11-20 15:29:23 +0100

    Update metrics.py
    Validated chamfer_distance by comparing with D. Colliaux implementation.
    Use of method based on `open3d` as it was found to be slightly faster that `cKDTree` from `scipy.spatial`.
    More documentations :D

--- romiscan/metrics.py
+++ romiscan/metrics.py
@@ -6,13 +6,12 @@ import numpy as np
 
 
 def chamfer_distance(ref_pcd, flo_pcd):
-    """
-    Compute the chamfer distance between two point clouds.
+    """Compute the symmetric chamfer distance between two point clouds.
 
     Let T be a template pointcloud and F a flaoting point cloud.
     Let p2p_dist be the Euclidian distance between two points t & f.
     The Chamfer distance is:
-    CH_dist = 1/|T| * sum_t(p2p_dist(t, f))
+    CH_dist = 1/|T| * sum_t(p2p_dist(t, f)) + 1/|F| * sum_f(p2p_dist(f, t))
 
     Parameters
     ----------
@@ -24,21 +23,79 @@ def chamfer_distance(ref_pcd, flo_pcd):
     Returns
     -------
     float
-        The chamfer distance.
+        The symmetric chamfer distance.
+
+    See Also
+    --------
+    open3d.geometry.PointCloud.compute_point_cloud_distance
+
+    Examples
+    --------
+    >>> import open3d as o3d
+    >>> import numpy as np
+    >>> from romiscan.metrics import chamfer_distance
+    >>> fpath_a = '/data/ROMI/20201119192731_rep_test_AnglesAndInternodes/arabido_test4_0/PointCloud__200_0_1_0_False_4ce2e46446/PointCloud.ply'
+    >>> fpath_b = '/data/ROMI/20201119192731_rep_test_AnglesAndInternodes/arabido_test4_1/PointCloud__200_0_1_0_False_4ce2e46446/PointCloud.ply'
+    >>> pcd_a = o3d.io.read_point_cloud(fpath_a)
+    >>> pcd_b = o3d.io.read_point_cloud(fpath_b)
+    >>> chamfer_distance(pcd_a, pcd_b)
 
     """
-    p2p_dist = ref_pcd.compute_point_cloud_distance(flo_pcd)
-    chamfer_dist = 1 / len(ref_pcd.points) * sum(p2p_dist)
+    p2p_dist_a = ref_pcd.compute_point_cloud_distance(flo_pcd)
+    p2p_dist_b = flo_pcd.compute_point_cloud_distance(ref_pcd)
+    chamfer_dist = 1 / len(ref_pcd.points) * sum(p2p_dist_a) + 1 / len(flo_pcd.points) * sum(p2p_dist_b)
     return chamfer_dist
 
 
 def point_cloud_registration_fitness(ref_pcd, flo_pcd, max_distance=2):
+    """Compute fitness & inliers RMSE after point-clouds registration.
+
+    Parameters
+    ----------
+    ref_pcd : open3d.geometry.PointCloud
+        Reference point cloud.
+    flo_pcd : open3d.geometry.PointCloud
+        Floating point cloud.
+    max_distance : float, optional
+        Maximum correspondence points-pair distance.
+        Default is `2.0`.
+
+    Returns
+    -------
+    float
+        The fitness between the two point clouds after registration.
+    float
+        The inlier RMSE between the two point clouds after registration.
+
+    See Also
+    --------
+    open3d.pipelines.registration.evaluate_registration
+
+    Examples
+    --------
+    >>> import open3d as o3d
+    >>> import numpy as np
+    >>> from romiscan.metrics import point_cloud_registration_fitness
+    >>> fpath_a = '/data/ROMI/20201119192731_rep_test_AnglesAndInternodes/arabido_test4_0/PointCloud__200_0_1_0_False_4ce2e46446/PointCloud.ply'
+    >>> fpath_b = '/data/ROMI/20201119192731_rep_test_AnglesAndInternodes/arabido_test4_1/PointCloud__200_0_1_0_False_4ce2e46446/PointCloud.ply'
+    >>> pcd_a = o3d.io.read_point_cloud(fpath_a)
+    >>> pcd_b = o3d.io.read_point_cloud(fpath_b)
+    >>> point_cloud_registration_fitness(pcd_a, pcd_b)
+
+    """
     res = o3d.pipelines.registration.evaluate_registration(ref_pcd, flo_pcd, max_distance)
     return res.fitness, res.inlier_rmse
 
 
 def set_metrics(mask_ref, mask_flo):
     """Compare two binary masks as sets.
+
+    Parameters
+    ----------
+    mask_ref : numpy.array
+        The reference binary mask.
+    mask_flo : numpy.array
+        The floating binary mask.
 
     Returns
     -------
@@ -54,6 +111,7 @@ def set_metrics(mask_ref, mask_flo):
         Precision as TP/(TP+FP)
     float
         Recall as TP/(TP+FN)
+
     """
     tp = int(np.sum(mask_ref * (mask_flo > 0)))
     fn = int(np.sum(mask_ref * (mask_flo == 0)))
@@ -71,14 +129,18 @@ def surface_ratio(ref_tmesh, flo_tmesh):
     flo_tmesh : open3d.geometry.TriangleMesh
         Target mesh for surface comparison.
 
-    Notes
-    -----
-    Requires ``open3d>=0.10.0``.
-
     Returns
     -------
     float
         The meshes surface ratio in [0, 1] with ``min_surf/max_surf``.
+
+    Notes
+    -----
+    Requires ``open3d>=0.10.0``.
+
+    See Also
+    --------
+    open3d.geometry.PointCloud.get_surface_area
 
     """
     ref_s = ref_tmesh.get_surface_area()
@@ -95,14 +157,18 @@ def volume_ratio(ref_tmesh, flo_tmesh):
     flo_tmesh : open3d.geometry.TriangleMesh
         Target mesh for volume comparison.
 
-    Notes
-    -----
-    Requires ``open3d>=0.11.0``.
-
     Returns
     -------
     float
         The meshes volume ratio in [0, 1] with ``min_vol/max_vol``.
+
+    Notes
+    -----
+    Requires ``open3d>=0.11.0``.
+
+    See Also
+    --------
+    open3d.geometry.PointCloud.get_volume
 
     """
     ref_v = ref_tmesh.get_volume()
