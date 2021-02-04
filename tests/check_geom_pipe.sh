9188b7df8f70721c9602ea2b15d3ce53e3a5375b
Nabil AIT TALEB <aittalebnabil@gmail.com>
2021-02-04 15:53:46 +0100

    new names for config files

--- tests/check_geom_pipe.sh
+++ tests/check_geom_pipe.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Set up the environment variable PYOPENCL_CTX
+export PYOPENCL_CTX='0'
+
 ##### Check geometric pipeline
 # 1. clean
-romi_run_task --config ../config/test_geom_pipe.toml Clean testdata/real_plant/ --local-scheduler
+romi_run_task --config ../config/geom_pipe_real.toml Clean testdata/real_plant/ --local-scheduler
 # 2. run pipeline
-romi_run_task --config ../config/test_geom_pipe.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
+romi_run_task --config ../config/geom_pipe_real.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
 
 echo ""
 print_task_info Colmap testdata/real_plant/
--- tests/check_ml_pipe.sh
+++ tests/check_ml_pipe.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set up the environment variable PYOPENCL_CTX
+export PYOPENCL_CTX='0'
+
 ##### Check machine learning pipeline
 # 1. download models
 MODEL_DIRECTORY="testdata/models/models"
@@ -8,29 +11,23 @@ if [ ! -d ${MODEL_DIRECTORY} ]; then
 	mkdir ${MODEL_DIRECTORY}
 fi
 
-MODEL_EPOCH_896_896_50="${MODEL_DIRECTORY}/Resnetdataset_gl_png_896_896_epoch50.pt"
+MODEL_EPOCH_896_896_50="${MODEL_DIRECTORY}/Resnet_896_896_epoch50.pt"
 
 if [ ! -f ${MODEL_EPOCH_896_896_50} ]; then
-	wget https://media.romi-project.eu/data/Resnetdataset_gl_png_896_896_epoch50.pt
-	mv Resnetdataset_gl_png_896_896_epoch50.pt ${MODEL_DIRECTORY}
+	wget https://media.romi-project.eu/data/Resnet_896_896_epoch50.pt
+	mv Resnet_896_896_epoch50.pt ${MODEL_DIRECTORY}
 fi
 
-#MODEL_TMP_40="${MODEL_DIRECTORY}/tmp_epoch40.pt"
-#if [ ! -f ${MODEL_TMP_40} ]; then
-#	wget https://media.romi-project.eu/data/tmp_epoch40.pt
-#	mv tmp_epoch40.pt ${MODEL_DIRECTORY}
-#fi
-
 # 2. clean
-romi_run_task --config ../config/test_ml_pipe.toml Clean testdata/real_plant/ --local-scheduler
+romi_run_task --config ../config/ml_pipe_real.toml Clean testdata/real_plant/ --local-scheduler
 # 3. run pipeline
-romi_run_task --config ../config/test_ml_pipe.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
+romi_run_task --config ../config/ml_pipe_real.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
 print_task_info PointCloud testdata/real_plant/
 print_task_info AnglesAndInternodes testdata/real_plant/
 
 # 4. clean
-romi_run_task --config ../config/ml_pipe_vplants_3.toml Clean testdata/virtual_plant/ --local-scheduler
+romi_run_task --config ../config/ml_pipe_virtual.toml Clean testdata/virtual_plant/ --local-scheduler
 # 5. run pipeline
-romi_run_task --config ../config/ml_pipe_vplants_3.toml AnglesAndInternodes testdata/virtual_plant/ --local-scheduler
+romi_run_task --config ../config/ml_pipe_virtual.toml AnglesAndInternodes testdata/virtual_plant/ --local-scheduler
 print_task_info PointCloud testdata/virtual_plant/
 print_task_info AnglesAndInternodes testdata/virtual_plant/
