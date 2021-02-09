romi/Scan3D

Update & enhance pipeline test scripts

tests/check_geom_pipe.sh
@@ -1,16 +1,131 @@
 #!/bin/bash
 
+# Default configuration file used for GEOMETRIC based pipeline is:
+cfg='../config/geom_pipe_test.toml'
+# Default database location:
+db='testdata'
+# Default test dataset for GEOMETRIC based pipeline is the "real_plant":
+dataset="$db/real_plant/"
+# Virtual test dataset:
+v_dataset="$db/virtual_plant/"
+# Default tested ROMI task for GEOMETRIC based pipeline is "AnglesAndInternodes":
+task='AnglesAndInternodes'
+
+usage(){
+  echo "USAGE:"
+  echo "  ./check_test_geom_pipe.sh [OPTIONS]
+  "
+  echo "DESCRIPTION:"
+  echo "  Clean the dataset then run 'romi_run_task <task> <dataset> --config <config>'.
+  Some information are printed for required task.
+  By default report for tasks 'Colmap', 'PointCloud' & 'AnglesAndInternodes'.
+  "
+  echo "OPTIONS:"
+  echo "  -c, --config
+    Pipeline configuration file to use, default to '$cfg'."
+  echo "  -d, --dataset
+    Data set to use, default to '$dataset'."
+  echo "  -t, --task
+    Task to test, default to '$task'."
+  echo "  --virtual
+    Use the virtual plant dataset."
+  echo "  --tmp
+    Clone the dataset to the temporary folder '/tmp' first.
+    Use this to avoid messing up your repository 'testdata' directory as the Clean task is not reliable."
+  echo "  -h, --help
+    Output a usage message and exit.
+  "
+  echo "EXAMPLES:"
+  echo "# Test geometric reconstruction pipeline on default real plant (safe mode):
+  $ check_geom_pipe.sh --tmp
+  "
+  echo "# Test CNN geometric pipeline up to the 'PointCloud' task on virtual plant (safe mode):
+  $ check_geom_pipe.sh -t PointCloud --virtual --tmp
+  "
+  echo "# Test geometric reconstruction pipeline with another config & dataset (safe mode):
+  $ check_geom_pipe.sh --config ../config/geom_pipe_test.toml --dataset /data/ROMI/DB/arabido_test2/ --tmp
+  "
+}
+tmp=0
+while [ "$1" != "" ]; do
+  case $1 in
+  -c | --config)
+    shift
+    cfg=$1
+    ;;
+  -d | --dataset)
+    shift
+    dataset=$1
+    ;;
+  -t | --task)
+    shift
+    task=$1
+    ;;
+  --virtual)
+    shift
+    dataset=$v_dataset
+    ;;
+  --tmp)
+    shift
+    tmp=1
+    ;;
+  -h | --help)
+    usage
+    exit
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+# If not defined, set 'PYOPENCL_CTX' to O
+if [ -z "$PYOPENCL_CTX" ]; then
+  export PYOPENCL_CTX='0'
+  echo "Missing 'PYOPENCL_CTX' environment variable, set it to '0'."
+fi
+
+# Create the copy to temporary folder
+if [ "$tmp" = 1 ]; then
+  tmp_db="/tmp/romidb"
+  # Create `romidb` folder as root to temporary database...
+  mkdir -p $tmp_db
+  # Add the romidb marker file
+  touch "$tmp_db/romidb"
+  # Get the directory name (last in hierarchy):
+  data_dir=`basename "$dataset"`
+  # Creates the temporary directory path variable with it
+  tmp_dataset="$tmp_db/$data_dir"
+  # Make sure it does not exist or remove it:
+  if [[ -d $tmp_dataset ]]; then
+    rm -rf $tmp_dataset
+  fi
+  # Copy the data
+  echo "Copying '$dataset' to '$tmp_db'..."
+  cp -R $dataset "$tmp_db"
+  # Finally replace the dataset location by the temporary one
+  dataset="$tmp_dataset"
+fi
+
 ##### Check geometric pipeline
 # 1. clean
-romi_run_task --config ../config/test_geom_pipe.toml Clean testdata/real_plant/ --local-scheduler
-# 2. run pipeline
-romi_run_task --config ../config/test_geom_pipe.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
+romi_run_task Clean $dataset --config $cfg
 
-echo ""
-print_task_info Colmap testdata/real_plant/
-echo "
-"
-print_task_info PointCloud testdata/real_plant/
-echo "
-"
-print_task_info AnglesAndInternodes testdata/real_plant/
+# 2. run pipeline
+echo "romi_run_task $task $dataset --config $cfg"
+romi_run_task $task $dataset --config $cfg
+
+# 3. print informations about tested task
+if [ "$task" = "AnglesAndInternodes" ]; then
+  # Also inform about Colmap and PointCloud tasks if tested task is "AnglesAndInternodes"
+  echo ""
+  print_task_info Colmap $dataset
+  echo "
+  "
+  print_task_info PointCloud $dataset
+  echo "
+  "
+fi
+print_task_info $task $dataset

tests/check_ml_pipe.sh
@@ -1,20 +1,134 @@
 #!/bin/bash
 
-##### Check machine learning pipeline
-# 1. download models
-MODEL_DIRECTORY="testdata/models/models"
+# Default configuration file used for CNN based pipeline is:
+cfg='../config/ml_pipe_test.toml'
+# Default database location:
+db='testdata'
+# Default test dataset for CNN based pipeline is the "real_plant":
+dataset="$db/real_plant/"
+# Virtual test dataset:
+v_dataset="$db/virtual_plant/"
+# Default tested ROMI task for CNN based pipeline is "AnglesAndInternodes":
+task='AnglesAndInternodes'
+# Directory with the trained organ segmentation models
+MODEL_DIRECTORY="models/models"
 
+usage(){
+  echo "USAGE:"
+  echo "  ./check_test_geom_pipe.sh [OPTIONS]
+  "
+  echo "DESCRIPTION:"
+  echo "  Clean the dataset then run 'romi_run_task <task> <dataset> --config <config>'.
+  Some information are printed for required task.
+  By default report for tasks 'Colmap', 'PointCloud' & 'AnglesAndInternodes'.
+
+  It will download a trained organ segmentation model if missing from the database.
+  "
+  echo "OPTIONS:"
+  echo "  -c, --config
+    Pipeline configuration file to use, default to '$cfg'."
+  echo "  -d, --dataset
+    Data set to use, default to '$dataset'."
+  echo "  -t, --task
+    Task to test, default to '$task'."
+  echo "  --virtual
+    Use the virtual plant dataset."
+  echo "  --tmp
+    Clone the dataset to the temporary folder '/tmp' first.
+    Use this to avoid messing up your repository 'testdata' directory as the Clean task is not reliable."
+  echo "  -h, --help
+    Output a usage message and exit.
+  "
+  echo "EXAMPLES:"
+  echo "# Test CNN reconstruction pipeline on default real plant (safe mode):
+  $ check_ml_pipe.sh --tmp
+  "
+  echo "# Test CNN reconstruction pipeline up to the 'PointCloud' task on virtual plant (safe mode):
+  $ check_ml_pipe.sh -t PointCloud --virtual --tmp
+  "
+  echo "# Test CNN reconstruction pipeline with another config & dataset (safe mode):
+  $ check_ml_pipe.sh --config ../config/ml_pipe_test.toml --dataset /data/ROMI/DB/arabido_test2/ --tmp
+  "
+}
+while [ "$1" != "" ]; do
+  case $1 in
+  -c | --config)
+    shift
+    cfg=$1
+    ;;
+  -d | --dataset)
+    shift
+    dataset=$1
+    ;;
+  -t | --task)
+    shift
+    task=$1
+    ;;
+  --virtual)
+    shift
+    dataset=$v_dataset
+    ;;
+  --tmp)
+    shift
+    tmp=1
+    ;;
+  -h | --help)
+    usage
+    exit
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+# If not defined, set 'PYOPENCL_CTX' to O
+if [ -z "$PYOPENCL_CTX" ]; then
+  export PYOPENCL_CTX='0'
+  echo "Missing 'PYOPENCL_CTX' environment variable, set it to '0'."
+fi
+
+# Create the copy to temporary folder
+if [ "$tmp" = 1 ]; then
+  tmp_db="/tmp/romidb"
+  # Create `romidb` folder as root to temporary database...
+  mkdir -p $tmp_db
+  # Add the romidb marker file
+  touch "$tmp_db/romidb"
+  # Get the directory name (last in hierarchy):
+  data_dir=`basename "$dataset"`
+  # Creates the temporary directory path variable with it
+  tmp_dataset="$tmp_db/$data_dir"
+  # Make sure it does not exist or remove it:
+  if [[ -d $tmp_dataset ]]; then
+    rm -rf $tmp_dataset
+  fi
+  # Copy the data
+  echo "Copying '$dataset' to '$tmp_db'..."
+  cp -R $dataset "$tmp_db"
+  # Copy the models fileset
+  echo "Copying '$db/models' to '$tmp_db/models'..."
+  cp -R "$db/models" "$tmp_db/."
+  # Finally replace the database & dataset location by the temporary ones
+  dataset="$tmp_dataset"
+  db=$tmp_db
+fi
+
+##### Check machine learning pipeline
+MODEL_DIRECTORY="$db/$MODEL_DIRECTORY"
+# 1. download models
 if [ ! -d ${MODEL_DIRECTORY} ]; then
-	mkdir ${MODEL_DIRECTORY}
+  mkdir -p ${MODEL_DIRECTORY}
+  echo "Created missing models directory: ${MODEL_DIRECTORY}."
 fi
 
 MODEL_EPOCH_896_896_50="${MODEL_DIRECTORY}/Resnetdataset_gl_png_896_896_epoch50.pt"
-
 if [ ! -f ${MODEL_EPOCH_896_896_50} ]; then
-	wget https://media.romi-project.eu/data/Resnetdataset_gl_png_896_896_epoch50.pt
-	mv Resnetdataset_gl_png_896_896_epoch50.pt ${MODEL_DIRECTORY}
+  echo 'Download missing trained CNN models...'
+  wget -P ${MODEL_DIRECTORY} https://media.romi-project.eu/data/Resnetdataset_gl_png_896_896_epoch50.pt
 fi
-
 #MODEL_TMP_40="${MODEL_DIRECTORY}/tmp_epoch40.pt"
 #if [ ! -f ${MODEL_TMP_40} ]; then
 #	wget https://media.romi-project.eu/data/tmp_epoch40.pt
@@ -22,15 +136,21 @@ fi
 #fi
 
 # 2. clean
-romi_run_task --config ../config/test_ml_pipe.toml Clean testdata/real_plant/ --local-scheduler
-# 3. run pipeline
-romi_run_task --config ../config/test_ml_pipe.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
-print_task_info PointCloud testdata/real_plant/
-print_task_info AnglesAndInternodes testdata/real_plant/
+romi_run_task Clean $dataset --config $cfg
 
-# 4. clean
-romi_run_task --config ../config/ml_pipe_vplants_3.toml Clean testdata/virtual_plant/ --local-scheduler
-# 5. run pipeline
-romi_run_task --config ../config/ml_pipe_vplants_3.toml AnglesAndInternodes testdata/virtual_plant/ --local-scheduler
-print_task_info PointCloud testdata/virtual_plant/
-print_task_info AnglesAndInternodes testdata/virtual_plant/
+# 3. run pipeline
+echo "romi_run_task $task $dataset --config $cfg"
+romi_run_task $task $dataset --config $cfg
+
+# 4.print informations about tested task
+if [ "$task" = "AnglesAndInternodes" ]; then
+  # Also inform about Colmap and PointCloud tasks if tested task is "AnglesAndInternodes"
+  echo ""
+  print_task_info Colmap $dataset
+  echo "
+  "
+  print_task_info PointCloud $dataset
+  echo "
+  "
+fi
+print_task_info $task $dataset

romi/plant-3d-vision

tests/check_geom_pipe.sh
@@ -1,19 +1,132 @@
 #!/bin/bash
 
-# Set up the environment variable PYOPENCL_CTX
-export PYOPENCL_CTX='0'
+# Default configuration file used for GEOMETRIC based pipeline is:
+cfg='../config/geom_pipe_real.toml'
+# Default database location:
+db='testdata'
+# Default test dataset for GEOMETRIC based pipeline is the "real_plant":
+dataset="$db/real_plant/"
+# Virtual test dataset:
+v_dataset="$db/virtual_plant/"
+# Default tested ROMI task for GEOMETRIC based pipeline is "AnglesAndInternodes":
+task='AnglesAndInternodes'
+
+usage(){
+  echo "USAGE:"
+  echo "  ./check_test_geom_pipe.sh [OPTIONS]
+  "
+  echo "DESCRIPTION:"
+  echo "  Clean the dataset then run 'romi_run_task <task> <dataset> --config <config>'.
+  Some information are printed for required task.
+  By default report for tasks 'Colmap', 'PointCloud' & 'AnglesAndInternodes'.
+  "
+  echo "OPTIONS:"
+  echo "  -c, --config
+    Pipeline configuration file to use, default to '$cfg'."
+  echo "  -d, --dataset
+    Data set to use, default to '$dataset'."
+  echo "  -t, --task
+    Task to test, default to '$task'."
+  echo "  --virtual
+    Use the virtual plant dataset."
+  echo "  --tmp
+    Clone the dataset to the temporary folder '/tmp' first.
+    Use this to avoid messing up your repository 'testdata' directory as the Clean task is not reliable."
+  echo "  -h, --help
+    Output a usage message and exit.
+  "
+  echo "EXAMPLES:"
+  echo "# Test geometric reconstruction pipeline on default real plant (safe mode):
+  $ check_geom_pipe.sh --tmp
+  "
+  echo "# Test CNN geometric pipeline up to the 'PointCloud' task on virtual plant (safe mode):
+  $ check_geom_pipe.sh -t PointCloud --virtual --tmp
+  "
+  echo "# Test geometric reconstruction pipeline with another config & dataset (safe mode):
+  $ check_geom_pipe.sh --config ../config/geom_pipe_real.toml --dataset /data/ROMI/DB/arabido_test2/ --tmp
+  "
+}
+tmp=0
+while [ "$1" != "" ]; do
+  case $1 in
+  -c | --config)
+    shift
+    cfg=$1
+    ;;
+  -d | --dataset)
+    shift
+    dataset=$1
+    ;;
+  -t | --task)
+    shift
+    task=$1
+    ;;
+  --virtual)
+    shift
+    dataset=$v_dataset
+    cfg='../config/geom_pipe_virtual.toml'
+    ;;
+  --tmp)
+    shift
+    tmp=1
+    ;;
+  -h | --help)
+    usage
+    exit
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+# If not defined, set 'PYOPENCL_CTX' to O
+if [ -z "$PYOPENCL_CTX" ]; then
+  export PYOPENCL_CTX='0'
+  echo "Missing 'PYOPENCL_CTX' environment variable, set it to '0'."
+fi
+
+# Create the copy to temporary folder
+if [ "$tmp" = 1 ]; then
+  tmp_db="/tmp/romidb"
+  # Create `romidb` folder as root to temporary database...
+  mkdir -p $tmp_db
+  # Add the romidb marker file
+  touch "$tmp_db/romidb"
+  # Get the directory name (last in hierarchy):
+  data_dir=`basename "$dataset"`
+  # Creates the temporary directory path variable with it
+  tmp_dataset="$tmp_db/$data_dir"
+  # Make sure it does not exist or remove it:
+  if [[ -d $tmp_dataset ]]; then
+    rm -rf $tmp_dataset
+  fi
+  # Copy the data
+  echo "Copying '$dataset' to '$tmp_db'..."
+  cp -R $dataset "$tmp_db"
+  # Finally replace the dataset location by the temporary one
+  dataset="$tmp_dataset"
+fi
 
 ##### Check geometric pipeline
 # 1. clean
-romi_run_task --config ../config/geom_pipe_real.toml Clean testdata/real_plant/ --local-scheduler
-# 2. run pipeline
-romi_run_task --config ../config/geom_pipe_real.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
+romi_run_task Clean $dataset --config $cfg
 
-echo ""
-print_task_info Colmap testdata/real_plant/
-echo "
-"
-print_task_info PointCloud testdata/real_plant/
-echo "
-"
-print_task_info AnglesAndInternodes testdata/real_plant/
+# 2. run pipeline
+echo "romi_run_task $task $dataset --config $cfg"
+romi_run_task $task $dataset --config $cfg
+
+# 3. print informations about tested task
+if [ "$task" = "AnglesAndInternodes" ]; then
+  # Also inform about Colmap and PointCloud tasks if tested task is "AnglesAndInternodes"
+  echo ""
+  print_task_info Colmap $dataset
+  echo "
+  "
+  print_task_info PointCloud $dataset
+  echo "
+  "
+fi
+print_task_info $task $dataset

tests/check_ml_pipe.sh
@@ -1,33 +1,152 @@
 #!/bin/bash
 
-# Set up the environment variable PYOPENCL_CTX
-export PYOPENCL_CTX='0'
+# Default configuration file used for CNN based pipeline is:
+cfg='../config/ml_pipe_real.toml'
+# Default database location:
+db='testdata'
+# Default test dataset for CNN based pipeline is the "real_plant":
+dataset="$db/real_plant/"
+# Virtual test dataset:
+v_dataset="$db/virtual_plant/"
+# Default tested ROMI task for CNN based pipeline is "AnglesAndInternodes":
+task='AnglesAndInternodes'
+# Directory with the trained organ segmentation models
+MODEL_DIRECTORY="models/models"
+
+usage(){
+  echo "USAGE:"
+  echo "  ./check_test_geom_pipe.sh [OPTIONS]
+  "
+  echo "DESCRIPTION:"
+  echo "  Clean the dataset then run 'romi_run_task <task> <dataset> --config <config>'.
+  Some information are printed for required task.
+  By default report for tasks 'Colmap', 'PointCloud' & 'AnglesAndInternodes'.
+
+  It will download a trained organ segmentation model if missing from the database.
+  "
+  echo "OPTIONS:"
+  echo "  -c, --config
+    Pipeline configuration file to use, default to '$cfg'."
+  echo "  -d, --dataset
+    Data set to use, default to '$dataset'."
+  echo "  -t, --task
+    Task to test, default to '$task'."
+  echo "  --virtual
+    Use the virtual plant dataset."
+  echo "  --tmp
+    Clone the dataset to the temporary folder '/tmp' first.
+    Use this to avoid messing up your repository 'testdata' directory as the Clean task is not reliable."
+  echo "  -h, --help
+    Output a usage message and exit.
+  "
+  echo "EXAMPLES:"
+  echo "# Test CNN reconstruction pipeline on default real plant (safe mode):
+  $ check_ml_pipe.sh --tmp
+  "
+  echo "# Test CNN reconstruction pipeline up to the 'PointCloud' task on virtual plant (safe mode):
+  $ check_ml_pipe.sh -t PointCloud --virtual --tmp
+  "
+  echo "# Test CNN reconstruction pipeline with another config & dataset (safe mode):
+  $ check_ml_pipe.sh --config ../config/ml_pipe_real.toml --dataset /data/ROMI/DB/arabido_test2/ --tmp
+  "
+}
+while [ "$1" != "" ]; do
+  case $1 in
+  -c | --config)
+    shift
+    cfg=$1
+    ;;
+  -d | --dataset)
+    shift
+    dataset=$1
+    ;;
+  -t | --task)
+    shift
+    task=$1
+    ;;
+  --virtual)
+    shift
+    dataset=$v_dataset
+    cfg='../config/ml_pipe_virtual.toml'
+    ;;
+  --tmp)
+    shift
+    tmp=1
+    ;;
+  -h | --help)
+    usage
+    exit
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+# If not defined, set 'PYOPENCL_CTX' to O
+if [ -z "$PYOPENCL_CTX" ]; then
+  export PYOPENCL_CTX='0'
+  echo "Missing 'PYOPENCL_CTX' environment variable, set it to '0'."
+fi
+
+# Create the copy to temporary folder
+if [ "$tmp" = 1 ]; then
+  tmp_db="/tmp/romidb"
+  # Create `romidb` folder as root to temporary database...
+  mkdir -p $tmp_db
+  # Add the romidb marker file
+  touch "$tmp_db/romidb"
+  # Get the directory name (last in hierarchy):
+  data_dir=`basename "$dataset"`
+  # Creates the temporary directory path variable with it
+  tmp_dataset="$tmp_db/$data_dir"
+  # Make sure it does not exist or remove it:
+  if [[ -d $tmp_dataset ]]; then
+    rm -rf $tmp_dataset
+  fi
+  # Copy the data
+  echo "Copying '$dataset' to '$tmp_db'..."
+  cp -R $dataset "$tmp_db"
+  # Copy the models fileset
+  echo "Copying '$db/models' to '$tmp_db/models'..."
+  cp -R "$db/models" "$tmp_db/."
+  # Finally replace the database & dataset location by the temporary ones
+  dataset="$tmp_dataset"
+  db=$tmp_db
+fi
 
 ##### Check machine learning pipeline
+MODEL_DIRECTORY="$db/$MODEL_DIRECTORY"
 # 1. download models
-MODEL_DIRECTORY="testdata/models/models"
-
 if [ ! -d ${MODEL_DIRECTORY} ]; then
-	mkdir ${MODEL_DIRECTORY}
+  mkdir -p ${MODEL_DIRECTORY}
+  echo "Created missing models directory: ${MODEL_DIRECTORY}."
 fi
 
 MODEL_EPOCH_896_896_50="${MODEL_DIRECTORY}/Resnet_896_896_epoch50.pt"
-
 if [ ! -f ${MODEL_EPOCH_896_896_50} ]; then
-	wget https://media.romi-project.eu/data/Resnet_896_896_epoch50.pt
-	mv Resnet_896_896_epoch50.pt ${MODEL_DIRECTORY}
+  echo 'Download missing trained CNN models...'
+  wget -P ${MODEL_DIRECTORY} https://media.romi-project.eu/data/Resnet_896_896_epoch50.pt
 fi
 
 # 2. clean
-romi_run_task --config ../config/ml_pipe_real.toml Clean testdata/real_plant/ --local-scheduler
-# 3. run pipeline
-romi_run_task --config ../config/ml_pipe_real.toml AnglesAndInternodes testdata/real_plant/ --local-scheduler
-print_task_info PointCloud testdata/real_plant/
-print_task_info AnglesAndInternodes testdata/real_plant/
+romi_run_task Clean $dataset --config $cfg
 
-# 4. clean
-romi_run_task --config ../config/ml_pipe_virtual.toml Clean testdata/virtual_plant/ --local-scheduler
-# 5. run pipeline
-romi_run_task --config ../config/ml_pipe_virtual.toml AnglesAndInternodes testdata/virtual_plant/ --local-scheduler
-print_task_info PointCloud testdata/virtual_plant/
-print_task_info AnglesAndInternodes testdata/virtual_plant/
+# 3. run pipeline
+echo "romi_run_task $task $dataset --config $cfg"
+romi_run_task $task $dataset --config $cfg
+
+# 4.print informations about tested task
+if [ "$task" = "AnglesAndInternodes" ]; then
+  # Also inform about Colmap and PointCloud tasks if tested task is "AnglesAndInternodes"
+  echo ""
+  print_task_info Colmap $dataset
+  echo "
+  "
+  print_task_info PointCloud $dataset
+  echo "
+  "
+fi
+print_task_info $task $dataset