c1a22465441d9f0dc1b45303b731547b900db18d
Jonathan LEGRAND <jlegrand62@gmail.com>
2021-02-18 17:59:57 +0100

    Fix TestColmap & DBTestCase

--- tests/test_colmap.py
+++ tests/test_colmap.py
@@ -21,7 +21,7 @@ class TestColmap(DBTestCase):
                 "--robust_alignment_max_error" : "10"
             }
         }
-        fileset = self.get_test_db().get_scan("arabidopsis000").get_fileset("images")
+        fileset = self.get_test_db("testdata").get_scan("arabidopsis000").get_fileset("images")
         runner = colmap.ColmapRunner(fileset, matcher, compute_dense, all_cli_args, align_pcd, True, fileset.scan.get_metadata("scanner")["workspace"])
         runner.run()
 
