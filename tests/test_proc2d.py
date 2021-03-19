762584ba39d4787b09cba902ed1721a0d50e7555
kodda <koddda@gmail.com>
2021-03-19 15:46:57 +0100

    remove vessalness test

--- tests/test_proc2d.py
+++ tests/test_proc2d.py
@@ -11,27 +11,5 @@ class TestProc2D(unittest.TestCase):
         y = proc2d.excess_green(x)
         assert(y.shape == (100, 100))
 
-    def test_hessian_eigvals_abs_sorted_3d(self):
-        test = np.zeros((3,3,3))
-        test[1,1,1] = 1.0
-        test[1,1,2] = 1.0
-        test[1,1,0] = 1.0
-        l1, l2, l3 = proc2d.hessian_eigvals_abs_sorted(test)
-        assert(np.sum(l2 <= l1) == 27)
-        assert(np.sum(l3 <= l2) == 27)
-
-    def test_hessian_eigvals_abs_sorted_2d(self):
-        test = np.zeros((3,3))
-        test[1,1] = 1.0
-        test[1,0] = 1.0
-        l1, l2 = proc2d.hessian_eigvals_abs_sorted(test)
-        assert(np.sum(l2 <= l1) == 9)
-
-    def test_vesselness(self):
-        test = np.zeros((3,3))
-        test[1,:] = 1.0
-        res = proc2d.vesselness(test, 1)
-
-
 if __name__ == "__main__":
     unittest.main()
