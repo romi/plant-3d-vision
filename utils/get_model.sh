33ee5f69679240fd8e536ae3af7d455ec6937651
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2022-01-18 18:26:05 +0100

    Fix get_model.sh

--- utils/get_model.sh
+++ utils/get_model.sh
@@ -4,7 +4,10 @@ model="Resnet_896_896_epoch50.pt"
 url="https://media.romi-project.eu/data/$model"
 path="plant-3d-vision/tests/testdata/models/models"
 
-if [[ -d "$path/$model" ]]; then
+if [[ -f "$path/$model" ]]; then
+  echo "Found model file $model"
+else
+  echo "Could not find model file $model, downloading it..."
   wget -nv --show-progress --progress=bar:force:noscroll $url
   mkdir -p $path
   mv $model $path
