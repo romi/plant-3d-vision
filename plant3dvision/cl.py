51438aea0ebb5f16ea6f4aff48093ca1523968c7
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2022-03-15 14:42:42 +0100

    Fix loggig levels & other minor fixes
    Add shebang & encoding declaration

--- plant3dvision/cl.py
+++ plant3dvision/cl.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """
 plant3dvision.cl
 ___________
@@ -5,7 +8,7 @@ ___________
 This module contains all OpenCL accelerated functions.
 The two main functionalities are:
 
-* Backprojection
+* Back-projection
 * Geodesics computing
 
 Geodesic computing is still in a very experimental stage.
@@ -14,9 +17,10 @@ import os
 
 import numpy as np
 import pyopencl as cl
-from plantdb import io
 from plant3dvision.log import logger
 from plant3dvision.proc3d import point2index
+
+from plantdb import io
 
 ctx = cl.create_some_context()
 queue = cl.CommandQueue(ctx)
@@ -27,12 +31,10 @@ prg_dir = os.path.join(os.path.dirname(__file__), 'kernels')
 eps = 1e-10
 
 with open(os.path.join(prg_dir, 'backprojection.c')) as f:
-    backprojection_kernels = cl.Program(
-        ctx, f.read()).build(options="-I%s" % prg_dir)
+    backprojection_kernels = cl.Program(ctx, f.read()).build(options="-I%s" % prg_dir)
 
 with open(os.path.join(prg_dir, 'geodesics.c')) as f:
-    geodesics_kernels = cl.Program(
-        ctx, f.read()).build(options="-I%s" % prg_dir)
+    geodesics_kernels = cl.Program(ctx, f.read()).build(options="-I%s" % prg_dir)
 
 with open(os.path.join(prg_dir, 'fim.c')) as f:
     fim_kernels = cl.Program(ctx, f.read()).build(options="-I%s" % prg_dir)
@@ -40,7 +42,7 @@ with open(os.path.join(prg_dir, 'fim.c')) as f:
 
 class Backprojection(object):
     """
-    This class implements backprojection onto a voxel volume.
+    This class implements back-projection onto a voxel volume.
 
     Attributes
     ----------
@@ -59,7 +61,7 @@ class Backprojection(object):
     dtype: {numpy.int32, numpy.float32}
         Data type used for the buffer, depends on initialization `type`.
     kernel: fun
-        Kernel to use for backprojection, depends on initialization `type`.
+        Kernel to use for back-projection, depends on initialization `type`.
 
     See Also
     --------
@@ -79,7 +81,7 @@ class Backprojection(object):
         voxel_size: float
             Size of voxels.
         type: {'carving', 'averaging'}
-            Method to use for backprojection, defaults to "carving".
+            Method to use for back-projection, defaults to "carving".
         default_value: float
             Default value when initializing the voxels (defaults to 0).
         labels: list
@@ -105,7 +107,7 @@ class Backprojection(object):
         buff_size = np.prod(self.shape) * 4
         logger.info(f"Buffer shape is {self.shape}")
         logger.info(f"Required memory for buffer is {buff_size} bytes!")
-        # Defines attributes used to initialize OpenCL buffers:
+        # Define attributes used to initialize OpenCL buffers:
         self.values_h = None
         self.values_d = None
         self.intrinsics_d = None
@@ -118,18 +120,13 @@ class Backprojection(object):
 
     def init_buffers(self):
         """Helper function to initialize OpenCL buffers."""
-        self.values_h = self.default_value * \
-                        np.ones(self.shape, dtype=self.dtype)
+        self.values_h = self.default_value * np.ones(self.shape, dtype=self.dtype)
 
-        self.values_d = cl.Buffer(
-            ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=self.values_h)
+        self.values_d = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=self.values_h)
 
-        self.intrinsics_d = cl.Buffer(
-            ctx, mf.READ_ONLY, np.zeros(4, dtype=np.float32).nbytes)
-        self.rot_d = cl.Buffer(
-            ctx, mf.READ_ONLY, np.zeros(9, dtype=np.float32).nbytes)
-        self.tvec_d = cl.Buffer(
-            ctx, mf.READ_ONLY, np.zeros(3, dtype=np.float32).nbytes)
+        self.intrinsics_d = cl.Buffer(ctx, mf.READ_ONLY, np.zeros(4, dtype=np.float32).nbytes)
+        self.rot_d = cl.Buffer(ctx, mf.READ_ONLY, np.zeros(9, dtype=np.float32).nbytes)
+        self.tvec_d = cl.Buffer(ctx, mf.READ_ONLY, np.zeros(3, dtype=np.float32).nbytes)
 
         self.volinfo_d = cl.Buffer(
             ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=np.array(
@@ -159,7 +156,7 @@ class Backprojection(object):
         if self.dtype == np.float32 and mask.dtype == np.uint8:
             logger.debug("type is uint8")
             mask = mask / 255
-        logger.critical(self.dtype)
+        # logger.critical(self.dtype)
         if self.log and self.dtype == np.float32:
             mask = np.log(eps + mask)
         intrinsics_h = np.ascontiguousarray(intrinsics).astype(np.float32)
@@ -195,8 +192,7 @@ class Backprojection(object):
             If `True` use the poses estimated by colmap (metadata `colmap_camera`),
             else should be defined in metadata `camera`. Default is `False`
         invert: bool
-            If `True`, default `False`, invert the values of the mask file to
-            process.
+            If `True`, default `False`, invert the values of the mask file to process.
 
         """
         if self.labels is not None:
@@ -204,12 +200,10 @@ class Backprojection(object):
             logger.debug(f"labels: {labels}")
             result = np.zeros((len(labels), *self.shape))
             for i, l in enumerate(labels):
-                result[i, :] = self.process_label(fs, l, use_colmap_poses,
-                                                  invert=invert)
+                result[i, :] = self.process_label(fs, l, use_colmap_poses, invert=invert)
             return result
         else:
-            return self.process_label(fs, use_colmap_poses=use_colmap_poses,
-                                      invert=invert)
+            return self.process_label(fs, use_colmap_poses=use_colmap_poses, invert=invert)
 
     def process_label(self, fs, label=None, use_colmap_poses=False,
                       invert=False):
@@ -245,8 +239,7 @@ class Backprojection(object):
                 cam = fi.get_metadata('camera')
 
             if cam is None:
-                logger.warning(
-                    "Could not get camera pose for view, skipping...")
+                logger.warning("Could not get camera pose for view, skipping...")
                 continue
 
             camera_model = cam["camera_model"]
@@ -276,7 +269,6 @@ class Backprojection(object):
         cl.enqueue_copy(queue, self.values_d, self.values_h)
 
 
-
 class Geodesics():
     def __init__(self):
         return
@@ -285,35 +277,27 @@ class Geodesics():
         shape = values.shape
         tips = point2index(tips, origin, voxel_size)
 
-        gx_d = cl.image_from_array(ctx, np.ascontiguousarray(
-            flow[:, :, :, 0]).astype(np.float32), 1)
-        gy_d = cl.image_from_array(ctx, np.ascontiguousarray(
-            flow[:, :, :, 1]).astype(np.float32), 1)
-        gz_d = cl.image_from_array(ctx, np.ascontiguousarray(
-            flow[:, :, :, 2]).astype(np.float32), 1)
+        gx_d = cl.image_from_array(ctx, np.ascontiguousarray(flow[:, :, :, 0]).astype(np.float32), 1)
+        gy_d = cl.image_from_array(ctx, np.ascontiguousarray(flow[:, :, :, 1]).astype(np.float32), 1)
+        gz_d = cl.image_from_array(ctx, np.ascontiguousarray(flow[:, :, :, 2]).astype(np.float32), 1)
 
         values_h = np.ascontiguousarray(values).astype(np.float32)
         values_d = cl.image_from_array(ctx, values_h, 1)
 
         points_h = np.ascontiguousarray(tips).astype(np.float32)
-        points_d = cl.Buffer(ctx, mf.READ_WRITE |
-                             mf.COPY_HOST_PTR, hostbuf=points_h)
+        points_d = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=points_h)
 
         labels_h = np.ones(tips.shape, dtype=np.uint8)
-        labels_d = cl.Buffer(ctx, mf.READ_WRITE |
-                             mf.COPY_HOST_PTR, hostbuf=labels_h)
+        labels_d = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=labels_h)
 
         votes_h = np.zeros(shape, dtype=np.int32)
-        votes_d = cl.Buffer(ctx, mf.READ_WRITE |
-                            mf.COPY_HOST_PTR, hostbuf=votes_h.ravel())
+        votes_d = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=votes_h.ravel())
 
         points_remain_h = np.asarray([True], dtype=np.int32)
-        points_remain_d = cl.Buffer(
-            ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=points_remain_h)
+        points_remain_d = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=points_remain_h)
 
         shape_h = np.array(shape, dtype=np.int32)
-        shape_d = cl.Buffer(ctx, mf.READ_WRITE |
-                            mf.COPY_HOST_PTR, hostbuf=shape_h)
+        shape_d = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=shape_h)
         idx = []
 
         for i in range(max_iters):
@@ -351,39 +335,33 @@ class FIM():
         pass
 
     def init_buffers(self):
-        self.speed = cl.Buffer(
-            ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.speed_h)
+        self.speed = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.speed_h)
         point_status_h = np.zeros(self.shape, dtype=np.int32)
 
-        self.active_pts = cl.Buffer(
-            ctx, mf.READ_WRITE, size=point_status_h.nbytes)
-        self.active_pts_aux = cl.Buffer(
-            ctx, mf.READ_WRITE, size=point_status_h.nbytes)
+        self.active_pts = cl.Buffer(ctx, mf.READ_WRITE, size=point_status_h.nbytes)
+        self.active_pts_aux = cl.Buffer(ctx, mf.READ_WRITE, size=point_status_h.nbytes)
 
-        self.point_status = cl.Buffer(
-            ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=point_status_h)
-        self.sol = cl.Buffer(
-            ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=np.inf*np.ones(self.shape, dtype=np.float32))
+        self.point_status = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=point_status_h)
+        self.sol = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR,
+                             hostbuf=np.inf * np.ones(self.shape, dtype=np.float32))
         shape_h = np.array(self.shape, dtype=np.int32)
-        self.shape_d = cl.Buffer(ctx, mf.READ_WRITE |
-                                 mf.COPY_HOST_PTR, hostbuf=shape_h)
+        self.shape_d = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=shape_h)
         self.n_active = 0
 
     def set_seeds(self, seeds):
         idx = point2index(seeds, self.origin, self.voxel_size)
 
-        flat_idx = idx[:, 0]*self.shape[1]*self.shape[2] + \
-            idx[:, 1]*self.shape[2] + idx[:, 2]
+        flat_idx = idx[:, 0] * self.shape[1] * self.shape[2] + idx[:, 1] * self.shape[2] + idx[:, 2]
         flat_idx = flat_idx.astype(np.int32)
         n_active = len(flat_idx)
-        status = 2*np.ones(n_active, dtype=np.int32)
+        status = 2 * np.ones(n_active, dtype=np.int32)
         self.n_active = n_active
 
         cl.enqueue_copy(queue, self.active_pts[:flat_idx.nbytes], flat_idx)
         cl.enqueue_copy(queue, self.point_status[:flat_idx.nbytes], status)
         for i in range(n_active):
             x = np.zeros(1, dtype=np.int32)
-            cl.enqueue_copy(queue, self.sol[flat_idx[i]*x.nbytes:(flat_idx[i]+1)*x.nbytes],
+            cl.enqueue_copy(queue, self.sol[flat_idx[i] * x.nbytes:(flat_idx[i] + 1) * x.nbytes],
                             np.zeros(1, dtype=np.int32))
 
         queue.finish()
@@ -391,18 +369,15 @@ class FIM():
     def run(self, steps=None):
         n_iter = 0
         cnt_h = np.zeros(1, dtype=np.int32)
-        cnt_d = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR,
-                          hostbuf=cnt_h)
+        cnt_d = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=cnt_h)
 
         has_converged_h = np.ones(1, dtype=np.int32)
-        has_converged_d = cl.Buffer(
-            ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=has_converged_h)
+        has_converged_d = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=has_converged_h)
         while True:
             if steps is not None and n_iter >= steps:
                 break
             queue.finish()
-            cl.enqueue_copy(queue, cnt_d, np.array(
-                [self.n_active], dtype=np.int32))
+            cl.enqueue_copy(queue, cnt_d, np.array([self.n_active], dtype=np.int32))
             self.kernel_add_neighbours(queue, (self.n_active,), None,
                                        self.active_pts, self.shape_d, self.point_status,
                                        np.int32(self.n_active), cnt_d)
@@ -426,8 +401,7 @@ class FIM():
 
             n_iter_update = 0
             while True:
-                cl.enqueue_copy(queue, has_converged_d,
-                                np.ones(1, dtype=np.int32))
+                cl.enqueue_copy(queue, has_converged_d, np.ones(1, dtype=np.int32))
                 self.kernel_update(queue, (self.n_active,), None,
                                    self.sol, self.shape_d, self.speed, self.active_pts, self.point_status,
                                    np.int32(self.n_active), np.float32(self.tol), has_converged_d)
@@ -452,8 +426,8 @@ class FIM():
         cl.enqueue_copy(queue, x, self.sol)
         queue.finish()
         gx, gy, gz = np.gradient(x)
-        n = np.sqrt(gx**2 + gy**2 + gz**2)
-        return gx/n, gy/n, gz/n
+        n = np.sqrt(gx ** 2 + gy ** 2 + gz ** 2)
+        return gx / n, gy / n, gz / n
 
 
 if __name__ == "__main__":
