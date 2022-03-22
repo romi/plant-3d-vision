5009596e18d0de6c72902754ac922cce6d20cea7
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2022-03-22 17:10:28 +0100

    Improve unittest for ColmapRunner
    - makes two test configs: CPU & GPU
    - disable use of calibration scan (not available in testdata!)

--- tests/unit/test_colmap.py
+++ tests/unit/test_colmap.py
@@ -1,33 +1,48 @@
 import unittest
-
-import numpy as np
-
-from os.path import join, abspath
+from os.path import abspath
+from os.path import join
 from pathlib import Path
+
 from plant3dvision import colmap
+
 from plantdb.testing import DBTestCase
 
 parent_dir = Path(__file__).resolve().parents[1]
 DATABASE_LOCATION = abspath(join(parent_dir, "testdata"))
 
+
 class TestColmap(DBTestCase):
-    def test_colmap(self):
+
+    def test_colmap_gpu(self):
         matcher = "exhaustive"
         compute_dense = False
         align_pcd = True
         all_cli_args = {
-            "feature_extractor" : {
-                "--ImageReader.single_camera" : "1",
-                "--SiftExtraction.use_gpu" : "0"
-            },
-            "exhaustive_matcher" : {
-            },
-            "model_aligner" : {
-                "--robust_alignment_max_error" : "10"
+            "feature_extractor": {
+                "--ImageReader.single_camera": "1",
             }
         }
         fileset = self.get_test_db(DATABASE_LOCATION).get_scan("arabidopsis000").get_fileset("images")
-        runner = colmap.ColmapRunner(fileset, matcher, compute_dense, all_cli_args, align_pcd, True, fileset.scan.get_metadata("scanner")["workspace"])
+        runner = colmap.ColmapRunner(fileset, matcher, compute_dense, all_cli_args, align_pcd, use_calibration=False,
+                                     bounding_box=fileset.scan.get_metadata("scanner")["workspace"])
+        runner.run()
+
+    def test_colmap_cpu(self):
+        matcher = "exhaustive"
+        compute_dense = False
+        align_pcd = True
+        all_cli_args = {
+            "feature_extractor": {
+                "--ImageReader.single_camera": "1",
+                "--SiftExtraction.use_gpu": "0"
+            },
+            "exhaustive_matcher": {
+                "--SiftMatching.use_gpu": "0"
+            }
+        }
+        fileset = self.get_test_db(DATABASE_LOCATION).get_scan("arabidopsis000").get_fileset("images")
+        runner = colmap.ColmapRunner(fileset, matcher, compute_dense, all_cli_args, align_pcd, use_calibration=False,
+                                     bounding_box=fileset.scan.get_metadata("scanner")["workspace"])
         runner.run()
 
 
