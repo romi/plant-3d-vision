cb78da893b85355fe7f41f234dbcc022c05153c6
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2022-04-28 12:26:17 +0200

    Add a `test_db_path` method.
    Returns the absolute path to local example/test database.

--- plant3dvision/__init__.py
+++ plant3dvision/__init__.py
@@ -1,2 +1,10 @@
-import shlex
-from subprocess import check_output
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+
+def test_db_path():
+    """Return the absolute path to local example/test database."""
+    root = Path(__file__).parent.parent
+    return root.joinpath('tests', 'testdata')
