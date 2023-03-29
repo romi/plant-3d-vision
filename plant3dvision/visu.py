74ba2a656e45bab879bd215ec0b3ff52664341cc
Jonathan LEGRAND <jlegrand62@gmail.com>
2023-03-29 02:00:07 +0200

    Improve plotly visu

--- plant3dvision/visu.py
+++ plant3dvision/visu.py
@@ -1,45 +1,244 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This module contains Plotly based rendering functions for 3D objects obtained using the reconstruction pipeline.
+
+These functions should be used in notebooks.
+"""
+
+import numpy as np
 import plotly.graph_objects as go
 
 
-def plotly_treegraph(tree, height=1000, title="Tree graph"):
-    """A Plotly representation of the tree graph.
+def plotly_pointcloud(pcd, n_pts=9000, height=900, width=900, title="Point cloud", marker_kwargs=None):
+    """A Plotly representation of the point cloud.
 
     Parameters
     ----------
-    tree : networkx.Graph
-        The tree graph.
+    pcd : open3d.geometry.PointCloud
+        The point cloud to render.
+    n_pts : int, optional
+        The number of point to display, defaults to `9000`.
+    height : int, optional
+        The height of the figure layout, default to `900`.
+    width : int, optional
+        The width of the figure layout, default to `900`.
+    title : str, optional
+        Title to add to the figure, default to `"Point cloud"`.
+    marker_kwargs : dict, optional
+        Marker styling dictionary, default to `{"size": 1, "color": 'green', "opacity": 0.8}`.
 
     Returns
     -------
     plotly.graph_objects.Figure
         The plotly figure to display.
-    """
-    points = {n: tree.nodes[n]['position'] for n in tree.nodes}
-    lines = [list(tree.edges)[j] for j in range(len(tree.edges))]
 
-    names = {}
-    for n in tree.nodes:
-        name = "_".join(tree.nodes[n]['labels'])
-        if name == "fruit":
-            name += f"_{tree.nodes[n]['fruit_id']}"
-        elif name == "stem":
-            name += f"_{tree.nodes[n]['main_stem_id']}"
-        names[n]=name
+    See Also
+    --------
+    plotly.graph_objects.Scatter3d
+    plotly.graph_objects.Figure
+
+    References
+    ----------
+    Plotly documentation for `Scatter3d`: https://plotly.com/python/reference/scatter3d/
+
+    """
+    if len(pcd.points) > n_pts:
+        rng = np.random.default_rng()
+        pcd_arr = rng.choice(pcd.points, 9000)
+    else:
+        pcd_arr = np.array(pcd.points)
+
+    marker_style = {"size": 1, "color": 'green', "opacity": 0.8}
+    if isinstance(marker_kwargs, dict):
+        marker_style.update(marker_kwargs)
+
+    x, y, z = pcd_arr.T
+    sc = go.Scatter3d(x=x, y=y, z=z, mode="markers", marker=marker_style)
+
+    fig = go.Figure(data=sc)
+    fig.update_layout(height=height, width=width, title=title, showlegend=False)
+    fig.update_scenes(aspectmode='data')
+
+    return fig
+
+
+def plotly_skeleton(skeleton, height=900, width=900, title="Skeleton", line_kwargs=None):
+    """A Plotly representation of the skeleton.
+
+    Parameters
+    ----------
+    skeleton : networkx.Graph
+        The skeleton to render.
+    height : int, optional
+        The height of the figure layout, default to `900`.
+    width : int, optional
+        The width of the figure layout, default to `900`.
+    title : str, optional
+        Title to add to the figure, default to `"Skeleton"`.
+    line_kwargs : dict, optional
+        Line styling dictionary, default to `{"size": 1, "color": 'green', "opacity": 0.8}`.
+
+    Returns
+    -------
+    plotly.graph_objects.Figure
+        The plotly figure to display.
+
+    See Also
+    --------
+    plotly.graph_objects.Scatter3d
+    plotly.graph_objects.Figure
+
+    References
+    ----------
+    Plotly documentation for `Scatter3d`: https://plotly.com/python/reference/scatter3d/
+
+    """
+    points = {n: skeleton.nodes[n]['position'] for n in skeleton.nodes}
+    lines = [list(skeleton.edges)[j] for j in range(len(skeleton.edges))]
+
+    line_style = {"width": 4}
+    if isinstance(line_kwargs, dict):
+        line_style.update(line_kwargs)
 
     lines_3d = []
     for line in lines:
         start, stop = line
         xt, yt, zt = points[start]
         xp, yp, zp = points[stop]
-        info = {start: names[start], stop: names[stop]}
-        htemplate = ["x: %{x}<br>" + "y: %{y}<br>" + "z: %{z}<br>" + f"node_id: {i}<br>type: {t}" for i, t in
-                     info.items()]
-        sc = go.Scatter3d(x=[xt, xp], y=[yt, yp], z=[zt, zp], mode='lines', line={"width": 4},
-                          hovertemplate=htemplate)
+        sc = go.Scatter3d(x=[xt, xp], y=[yt, yp], z=[zt, zp], mode='lines', line=line_style)
         lines_3d.append(sc)
 
     fig = go.Figure(data=lines_3d)
-    fig.update_layout(height=height, title=title, showlegend=False)
+    fig.update_layout(height=height, width=width, title=title, showlegend=False)
     fig.update_scenes(aspectmode='data')
 
     return fig
+
+
+def plotly_treegraph(tree, height=900, width=900, title="Tree graph", mode="lines", line_kwargs=None, marker_kwargs=None):
+    """A Plotly representation of the tree graph.
+
+    Parameters
+    ----------
+    tree : networkx.Graph
+        The tree graph to render.
+    height : int, optional
+        The height of the figure layout, default to `900`.
+    width : int, optional
+        The width of the figure layout, default to `900`.
+    title : str, optional
+        Title to add to the figure, default to `"Point cloud"`.
+    mode : {"lines", "markers", "lines+markers"}, optional
+        The type of representation to use for the stem and fruit nodes (markers) & edges (lines).
+        Defaults to `'lines'`.
+    line_kwargs : dict, optional
+        Line styling dictionary, default to `{"size": 4}`, blue for main stem and cycling colors for fruits.
+    marker_kwargs : dict, optional
+        Marker styling dictionary, default to `None`.
+
+    Returns
+    -------
+    plotly.graph_objects.Figure
+        The plotly figure to display.
+
+    See Also
+    --------
+    plotly.graph_objects.Scatter3d
+    plotly.graph_objects.Figure
+
+    References
+    ----------
+    Plotly documentation for `Scatter3d`: https://plotly.com/python/reference/scatter3d/
+
+    """
+    go_data = []
+    # - Add a point (diamond) to the root position
+    from plant3dvision.tree import get_root_node_id
+    try:
+        root_id = get_root_node_id(tree)
+    except:
+        pass
+    else:
+        x, y, z = tree.nodes[root_id]["position"]
+        root_sc = go.Scatter3d(x=[x], y=[y], z=[z], mode='markers',
+                               marker={"size": 4, "color": "blue", "symbol": "diamond"},
+                               name=f"root")
+        go_data.append(root_sc)
+
+    # - Construct the main stem scatter line:
+    from plant3dvision.tree import get_ordered_stem_nodes
+    from plant3dvision.tree import nodes_coordinates
+    main_stem_nodes = get_ordered_stem_nodes(tree)
+    # Get the main stem nodes coordinate:
+    main_stem_coords = nodes_coordinates(tree, main_stem_nodes)
+    # Create the hover template:
+    main_stem_ht = ["x: %{x}<br>" + "y: %{y}<br>" + "z: %{z}<br>" +
+                    f"node_id: {i}<br>type: main stem" for i in main_stem_nodes]
+    marker_style = {}
+    if isinstance(marker_kwargs, dict):
+        marker_style.update(marker_kwargs)
+    line_style = {"width": 4, "color": "blue"}
+    if isinstance(line_kwargs, dict):
+        line_style.update(line_kwargs)
+    # Create the scatter representation:
+    x, y, z = main_stem_coords.T
+    main_stem_sc = go.Scatter3d(x=x, y=y, z=z, mode=mode,
+                                line=line_style, marker=marker_style,
+                                name="main stem", hovertemplate=main_stem_ht)
+    go_data.append(main_stem_sc)
+
+    # - Construct a scatter line per fruit:
+    from plant3dvision.tree import get_ordered_branching_point_nodes
+    from plant3dvision.tree import select_fruit_nodes
+    bp_ids = get_ordered_branching_point_nodes(tree)
+    for bp_id in bp_ids:
+        fruit_nodes = select_fruit_nodes(tree, bp_id, max_node_dist=None)
+        if len(fruit_nodes) == 0:
+            continue
+        elif len(fruit_nodes) > 1:
+            # If more than one fruit, iterate & add a suffix to fruit id:
+            for n, fnodes in enumerate(fruit_nodes):
+                go_data.append(_fruit_sc(tree, bp_id, fnodes, mode=mode,
+                                         line_kwargs=line_kwargs,
+                                         marker_kwargs=marker_kwargs,
+                                         suffix=f"-{n}"))
+        else:
+            go_data.append(_fruit_sc(tree, bp_id, fruit_nodes[0], mode=mode,
+                                     line_kwargs=line_kwargs,
+                                     marker_kwargs=marker_kwargs,
+                                     suffix=""))
+
+    fig = go.Figure(data=go_data)
+    fig.update_layout(height=height, width=width, title=title, showlegend=True)
+    fig.update_scenes(aspectmode='data')
+
+    return fig
+
+
+def _fruit_sc(tree, bp_id, fruit_nodes, mode='lines', suffix="", line_kwargs=None, marker_kwargs=None):
+    from plant3dvision.tree import nodes_coordinates
+    fruit_nodes = [bp_id] + fruit_nodes
+    # Get the fruit id from the list of neighbors for the given branching point.
+    bp_nei = tree.neighbors(bp_id)
+    fid = [tree.nodes[nei]['fruit_id'] for nei in bp_nei if "fruit" in tree.nodes[nei]['labels']][0]
+    # Get the fruit nodes coordinate:
+    fruit_coords = nodes_coordinates(tree, fruit_nodes)
+    x, y, z = fruit_coords.T
+    # Create the hover template:
+    fruit_ht = ["x: %{x}<br>" + "y: %{y}<br>" + "z: %{z}<br>" +
+                f"node_id: {i}<br>type: fruit {fid}{suffix}" for i in fruit_nodes]
+
+    marker_style = {}
+    if isinstance(marker_kwargs, dict):
+        marker_style.update(marker_kwargs)
+    line_style = {"width": 4}
+    if isinstance(line_kwargs, dict):
+        line_style.update(line_kwargs)
+
+    # Create the scatter representation:
+    fruit_sc = go.Scatter3d(x=x, y=y, z=z, mode=mode,
+                            line=line_style, marker=marker_style,
+                            name=f"fruit {fid}{suffix}", hovertemplate=fruit_ht)
+    return fruit_sc
