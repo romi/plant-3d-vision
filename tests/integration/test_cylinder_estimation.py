b5e46ec41ef3454a121d7e162558c7a62dce37c6
Jonathan LEGRAND <jlegrand62@gmail.com>
2023-06-08 04:44:59 +0200

    Fix cylinder integration tests.

--- tests/integration/test_cylinder_estimation.py
+++ tests/integration/test_cylinder_estimation.py
@@ -1,13 +1,9 @@
 import unittest
-from pathlib import Path
-import os
-import json
-import glob
-import requests
 
 from plantdb.fsdb import dummy_db
 from plantdb.io import read_json
 from utilities import run_task
+
 
 class CylinderRadiusGroundTruth(unittest.TestCase):
     def test_cylinder(self):
@@ -16,7 +12,7 @@ class CylinderRadiusGroundTruth(unittest.TestCase):
 
         # Perform the CylinderRadiusGroundTruth task
         process = run_task("CylinderRadiusGroundTruth", str(db.path() / "myscan_001"))
-        assert(process.returncode == 0)
+        self.assertTrue(process.returncode == 0)
 
         db.connect()
         scan = db.get_scan("myscan_001")
@@ -30,7 +26,8 @@ class CylinderRadiusGroundTruth(unittest.TestCase):
         self.assertIsNotNone(gt_fs.get_metadata("nb_points"))
 
         gt_ply = scan.get_fileset(fs_id).get_file("CylinderRadiusGroundTruth")
-        assert gt_ply.path().exists()
+        self.assertTrue(gt_ply.path().exists())
+
 
 class TestCylinderRadiusEstimation(unittest.TestCase):
     def test_cylinder(self):
@@ -39,7 +36,7 @@ class TestCylinderRadiusEstimation(unittest.TestCase):
 
         # Perform the CylinderRadiusEstimation task
         process = run_task("CylinderRadiusEstimation", str(db.path() / "myscan_001"))
-        assert(process.returncode == 0)
+        self.assertTrue(process.returncode == 0)
 
         db.connect()
         scan = db.get_scan("myscan_001")
@@ -50,10 +47,11 @@ class TestCylinderRadiusEstimation(unittest.TestCase):
         # Get predicted radius from task JSON output:
         fs_id = scan.list_filesets(query={"task_name": "CylinderRadiusEstimation"})[0]
         pred_json = scan.get_fileset(fs_id).get_file("CylinderRadiusEstimation")
-        assert pred_json.path().exists()
+        self.assertTrue(pred_json.path().exists())
         pred_radius = read_json(pred_json)['calculated_radius']
 
         self.assertAlmostEqual(gt_radius, pred_radius, delta=0.1)
+
 
 if __name__ == '__main__':
     unittest.main()
