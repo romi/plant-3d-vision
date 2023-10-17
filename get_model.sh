4e492f25301620c12cfc5877c521a9125d67c8ec
Jonathan Legrand <jlegrand62@gmail.com>
2023-10-17 18:53:48 +0200

    Improve `get_model.sh` inline doc.

--- get_model.sh
+++ get_model.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# - Defines colors and message types:
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+NC="\033[0m" # No Color
+INFO="${GREEN}INFO${NC}    "
+WARNING="${YELLOW}WARNING${NC} "
+
 model="Resnet_896_896_epoch50.pt"
-url="https://media.romi-project.eu/data/$model"
+url="https://media.romi-project.eu/data/${model}"
 path="tests/testdata/models/models"
 
-if [[ -f "$path/$model" ]]; then
-  echo "Found model file $model"
+if [[ -f "${path}/${model}" ]]; then
+  echo -e "${INFO}Found trained CNN model file '${model}'."
 else
-  echo "Could not find model file $model, downloading it..."
-  wget -nv --show-progress --progress=bar:force:noscroll $url
-  mkdir -p $path
-  mv $model $path
+  echo -e "${WARNING}Could not find trained CNN model file '${model}'!"
+  echo -e "${INFO}Downloading it..."
+  wget -nv --show-progress --progress=bar:force:noscroll ${url}
+  mkdir -p ${path}
+  mv ${model} ${path}
 fi
