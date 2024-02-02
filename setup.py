3e1cb73622880e682f21ce3cc8f8b1c754978dc0
Jonathan Legrand <jlegrand62@gmail.com>
2024-02-02 15:34:59 +0100

    Update `setup.py`.

--- setup.py
+++ setup.py
@@ -5,8 +5,27 @@ with open("README.md", "r") as fh:
 
 opts = dict(
     name='plant3dvision',
-    packages=find_packages(),
     version='0.12.99',
+    description='3D reconstruction & quantification of single potted plants from RGB scans.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Timothée Wintz',
+    author_email='timothee@timwin.fr',
+    maintainer='Jonathan Legrand',
+    maintainer_email='jonathan.legrand@ens-lyon.fr',
+    url="https://docs.romi-project.eu/Scanner/home/",
+    download_url='',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)"
+    ],
+    license="LGPL-3.0",
+    license_files='LICENSE',
+    keywords=['ROMI', 'photogrammetry', 'COLMAP'],
+    platforms=['linux'],
     scripts=[
         'bin/check_measures',
         'bin/colmap_poses',
@@ -15,25 +34,10 @@ opts = dict(
         'bin/robustness_evaluation',
         'bin/voronoi_texture_generator',
     ],
-    author='Timothée Wintz',
-    author_email='timothee@timwin.fr',
-    description='A plant 3d vision tool',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://docs.romi-project.eu/Scanner/home/",
     zip_safe=False,
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
     install_requires=[],  # see `requirements.txt`
     python_requires='>=3.8',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)"
-    ],
-    # If True, include any files specified by your MANIFEST.in:
-    include_package_data=True
+    include_package_data=True,   # if `True`, include any files specified by your `MANIFEST.in`
 )
 
 if __name__ == '__main__':
