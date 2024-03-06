ca466daeebd3698f382057ab40ab5e1fa34b47af
Jonathan Legrand <jlegrand62@gmail.com>
2024-03-06 22:28:16 +0100

    Bump to version 0.13

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 opts = dict(
     name='plant3dvision',
-    version='0.12.99',
+    version='0.13',
     description='3D reconstruction & quantification of single potted plants from RGB scans.',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -37,7 +41,7 @@ opts = dict(
     zip_safe=False,
     install_requires=[],  # see `requirements.txt`
     python_requires='>=3.8',
-    include_package_data=True,   # if `True`, include any files specified by your `MANIFEST.in`
+    include_package_data=True,  # if `True`, include any files specified by your `MANIFEST.in`
 )
 
 if __name__ == '__main__':
