4724a8a145246312e6d8cb8f6dfc0e74ee3f33a3
Jonathan Legrand <jlegrand62@gmail.com>
2025-08-11 23:20:30 +0200

    Add support for virtual environment in Docker setup
    - Fix incorrect assignment of `PYCUDA_DEFAULT_NVCC_FLAGS`.
    - Install `python3-venv` package in the Dockerfile.
    - Update build script to create and activate a Python virtual environment.
    - Modify `entrypoint.sh` to source the virtual environment before executing commands.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,7 @@
 
 # Change `umask` to be able to write files and directory with 'r+w' as group rights (else just 'r'):
 umask 0002
+# Activate the virtual environment
+source /home/romi/venv/bin/activate
+# Execute whatever command was passed to the container
 /bin/bash -c "$@"
