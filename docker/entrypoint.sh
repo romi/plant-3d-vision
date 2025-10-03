4b29ab1238a9ecd70497f096a51bacde312fb9d6
Jonathan Legrand <jlegrand62@gmail.com>
2025-10-03 10:36:32 +0200

    Add comments explaining new directory (775) and file (664) permission settings

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Change `umask` to be able to write files and directory with 'r+w' as group rights (else just 'r'):
+# - New directories will have permissions 775 (rwxrwxr-x)
+# - New files will have permissions 664 (rw-rw-r--)
 umask 0002
 # Activate the virtual environment
 source /home/romi/venv/bin/activate
