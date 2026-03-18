b41028dab280ef4e0c8fac95a06d68bbdb63300d
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2026-03-18 17:42:53 +0100

    Fix `colorspace` parameter definition in `proc2d.py`

--- plant3dvision/tasks/proc2d.py
+++ plant3dvision/tasks/proc2d.py
@@ -322,7 +322,7 @@ class Masks(FileByFileTask):
     """
     upstream_task = luigi.TaskParameter(default=Undistort)  # override default attribute from ``RomiTask``
     type = luigi.Parameter("linear")
-    colorspace = luigi.ChoiceParameter(str, choices=["RGB", "HSV", "YCbCr"], default="RGB")
+    colorspace = luigi.ChoiceParameter("RGB", choices=["RGB", "HSV", "YCbCr"])
     parameters = luigi.ListParameter(default=[0, 1, 0])
     min_threshold = luigi.FloatParameter(default=0.0)
     max_threshold = luigi.FloatParameter(default=0.4)
