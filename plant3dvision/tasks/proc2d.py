dafc5ebeb3cd5af9a496b44683a4301f36c0e40a
Jonathan Legrand <jonathan.legran@ens-lyon.fr>
2026-04-03 14:20:27 +0200

    changed colorspace parameter in tasks/proc2d.py to correctly understand 'RGB' as the default and not the type following luigi update

--- plant3dvision/tasks/proc2d.py
+++ plant3dvision/tasks/proc2d.py
@@ -322,7 +322,7 @@ class Masks(FileByFileTask):
     """
     upstream_task = luigi.TaskParameter(default=Undistort)  # override default attribute from ``RomiTask``
     type = luigi.Parameter("linear")
-    colorspace = luigi.ChoiceParameter("RGB", choices=["RGB", "HSV", "YCbCr"])
+    colorspace = luigi.ChoiceParameter(default="RGB", choices=["RGB", "HSV", "YCbCr"], var_type=str)
     parameters = luigi.ListParameter(default=[0, 1, 0])
     min_threshold = luigi.FloatParameter(default=0.0)
     max_threshold = luigi.FloatParameter(default=0.4)
