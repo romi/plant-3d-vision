8742df66021b828577d4bb076b113f56f5aa3d62
Jonathan Legrand <jlegrand62@gmail.com>
2026-05-12 23:23:35 +0200

    Use direct `Voxels` import and clean up task defaults
    - Import `Voxels` from `plant3dvision.tasks.voxel_reconstruction` and remove legacy `cl.Voxels` references throughout `evaluation.py`
    - Strip obsolete “override default attribute” comments from `luigi.TaskParameter` definitions in several tasks

--- plant3dvision/tasks/evaluation.py
+++ plant3dvision/tasks/evaluation.py
@@ -16,6 +16,7 @@ from plant3dvision.proc3d import PointCloudColorMap
 from plant3dvision.tasks import proc2d
 from plant3dvision.tasks import proc3d
 from plant3dvision.tasks.arabidopsis import AnglesAndInternodes
+from plant3dvision.tasks.voxel_reconstruction import Voxels
 from plantdb.commons.io import read_json
 from plantdb.commons.io import read_npz
 from plantdb.commons.io import read_point_cloud
@@ -26,10 +27,10 @@ from plantdb.commons.io import write_point_cloud
 from plantdb.commons.io import write_triangle_mesh
 from plantdb.commons.utils import to_file
 from romitask.log import get_logger
-from romitask.task import ScanConfiguration
 from romitask.task import FilesetTarget
 from romitask.task import ImagesFilesetExists
 from romitask.task import RomiTask
+from romitask.task import ScanConfiguration
 from romitask.task import Segmentation2DGroundTruthFilesetExists
 from romitask.task import VirtualPlantObj
 
@@ -80,7 +81,7 @@ class VoxelsGroundTruth(RomiTask):
         If unspecified (default), the current active scan will be used.
 
     """
-    upstream_task = luigi.TaskParameter(default=VirtualPlantObj)  # override default attribute from ``RomiTask``
+    upstream_task = luigi.TaskParameter(default=VirtualPlantObj)
 
     def run(self):
         import pywavefront
@@ -97,7 +98,7 @@ class VoxelsGroundTruth(RomiTask):
             res = {}
             min = np.min(x.vertices, axis=0)
             max = np.max(x.vertices, axis=0)
-            arr_size = np.asarray((max - min) / cl.Voxels().voxel_size + 1, dtype=int) + 1
+            arr_size = np.asarray((max - min) / Voxels().voxel_size + 1, dtype=int) + 1
             for k in x.meshes.keys():
                 t = o3d.geometry.TriangleMesh()
                 t.triangles = o3d.utility.Vector3iVector(np.asarray(x.meshes[k].faces))
@@ -106,11 +107,11 @@ class VoxelsGroundTruth(RomiTask):
                 o3d.write_triangle_mesh(os.path.join(tmpdir, "tmp.stl"),
                                         t)
                 m = trimesh.load(os.path.join(tmpdir, "tmp.stl"))
-                v = m.voxelized(cl.Voxels().voxel_size)
+                v = m.voxelized(Voxels().voxel_size)
 
                 class_name = x.meshes[k].materials[0].name
                 arr = np.zeros(arr_size)
-                voxel_size = cl.Voxels().voxel_size
+                voxel_size = Voxels().voxel_size
                 origin_idx = np.asarray((v.origin - min) / voxel_size, dtype=int)
                 arr[origin_idx[0]:origin_idx[0] + v.matrix.shape[0],
                 origin_idx[1]:origin_idx[1] + v.matrix.shape[1],
@@ -145,7 +146,7 @@ class PointCloudGroundTruth(RomiTask):
         Defaults to `100000`.
 
     """
-    upstream_task = luigi.TaskParameter(default=VirtualPlantObj)  # override default attribute from ``RomiTask``
+    upstream_task = luigi.TaskParameter(default=VirtualPlantObj)
     pcd_size = luigi.IntParameter(default=100000)
 
     def run(self):
@@ -207,7 +208,7 @@ class ClusteredMeshGroundTruth(RomiTask):
         If unspecified (default), the current active scan will be used.
 
     """
-    upstream_task = luigi.TaskParameter(default=VirtualPlantObj)  # override default attribute from ``RomiTask``
+    upstream_task = luigi.TaskParameter(default=VirtualPlantObj)
 
     def run(self):
         import pywavefront
@@ -311,8 +312,8 @@ class PointCloudEvaluation(EvaluationTask):
     --------
     open3d.cuda.pybind.pipelines.registration.evaluate_registration
     """
-    upstream_task = luigi.TaskParameter(default=proc3d.PointCloud)  # override default attribute from ``RomiTask``
-    ground_truth = luigi.TaskParameter(default=PointCloudGroundTruth)  # override default attribute from ``EvaluationTask``
+    upstream_task = luigi.TaskParameter(default=proc3d.PointCloud)
+    ground_truth = luigi.TaskParameter(default=PointCloudGroundTruth)
     max_distance = luigi.FloatParameter(default=2)
 
     def evaluate(self):
@@ -378,7 +379,8 @@ class Segmentation2DEvaluation(EvaluationTask):
     plant3dvision.metrics.CompareMaskFilesets
     """
     upstream_task = luigi.TaskParameter(default=proc2d.Segmentation2D)  # override default attribute from ``RomiTask``
-    ground_truth = luigi.TaskParameter(default=Segmentation2DGroundTruthFilesetExists)  # override default attribute from ``EvaluationTask``
+    ground_truth = luigi.TaskParameter(
+        default=Segmentation2DGroundTruthFilesetExists)  # override default attribute from ``EvaluationTask``
     dilation_amount = luigi.IntParameter(default=0)
     labels = luigi.ListParameter(default=[])
 
@@ -414,7 +416,7 @@ class VoxelsEvaluation(EvaluationTask):
     --------
     plant3dvision.metrics.CompareMaskFilesets
     """
-    upstream_task = luigi.TaskParameter(default=cl.Voxels)  # override default attribute from ``RomiTask``
+    upstream_task = luigi.TaskParameter(default=Voxels)  # override default attribute from ``RomiTask``
     ground_truth = luigi.TaskParameter(default=VoxelsGroundTruth)  # override default attribute from ``EvaluationTask``
 
     def evaluate(self):
@@ -446,8 +448,7 @@ class VoxelsEvaluation(EvaluationTask):
             prediction_c = prediction_c * (pred_c > (10 * pred_no_c))
 
             gt_c = gts[c]
-            gt_c = gt_c[0:prediction_c.shape[0], 0:prediction_c.shape[1],
-                   0:prediction_c.shape[2]]
+            gt_c = gt_c[0:prediction_c.shape[0], 0:prediction_c.shape[1], 0:prediction_c.shape[2]]
 
             fig = plt.figure()
             plt.subplot(2, 1, 1)
