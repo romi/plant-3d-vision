921bc55a9ad804aa86912d542945296d2173b82f
Jonathan Legrand <jlegrand62@gmail.com>
2026-06-02 22:55:07 +0200

    Rename `type` parameter to `method` in Proc2D task
    - Change `type = luigi.Parameter("linear")` to `method = luigi.Parameter("linear")` in `plant3dvision/tasks/proc2d.py` to avoid name clash with the built‑in `type` and better describe its purpose.

--- plant3dvision/tasks/proc2d.py
+++ plant3dvision/tasks/proc2d.py
@@ -418,7 +418,7 @@ class Masks(ParallelFileTask):
 
     """
     upstream_task = luigi.TaskParameter(default=Undistort)  # override default attribute from ``RomiTask``
-    type = luigi.Parameter("linear")
+    method = luigi.Parameter("linear")
     colorspace = luigi.ChoiceParameter("RGB", choices=["RGB", "HSV", "YCbCr"])
     parameters = luigi.ListParameter(default=[0, 1, 0])
     min_threshold = luigi.FloatParameter(default=0.0)
