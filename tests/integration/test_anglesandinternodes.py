1f38d235ee97b1965520a97d48205c89e080c3b3
Jonathan Legrand <jlegrand62@gmail.com>
2026-07-22 09:06:38 +0200

    Refactor integration tests: import `toml` and standardize dataset variable
    - Update all `run_task` calls, path constructions, and `get_models_dataset` invocation to use `plant_dataset`.
    - Revise comments to reference “pipeline” instead of “pipe”.
    - In `test_real_plant_custom_matcher`, add a print statement about modifying the configuration and run the task with the edited `custom_config`.

--- tests/integration/test_anglesandinternodes.py
+++ tests/integration/test_anglesandinternodes.py
@@ -4,7 +4,7 @@ import os
 import unittest
 from pathlib import Path
 
-from pydantic_settings.sources.providers import toml
+import toml
 
 from plantdb.commons.test_database import get_models_dataset
 from romitask.cli.romi_run_task import run_task
@@ -15,19 +15,19 @@ class TestGeomAnglesAndInternodes(unittest.TestCase):
     def test_real_plant(self):
         geom_pipe_real_conf = os.path.join(Path(__file__).parents[2], "configs/test_geom_pipe_real.toml")
         print(f"Testing geometric pipeline with conf: {geom_pipe_real_conf}")
-        real_plant_data = os.path.join(Path(__file__).parents[1], "testdata/real_plant/")
-        print(f"Testing geometric pipeline with data: {real_plant_data}")
+        plant_dataset = os.path.join(Path(__file__).parents[1], "testdata/real_plant/")
+        print(f"Testing geometric pipeline with data: {plant_dataset}")
 
-        # Perform a Clean before running the pipe
-        process = run_task(real_plant_data, "Clean", geom_pipe_real_conf, no_auth=True)
+        # Perform a Clean before running the pipeline
+        process = run_task(plant_dataset, "Clean", geom_pipe_real_conf, no_auth=True)
         self.assertTrue(process.returncode == 0)
 
         # Perform the AnglesAndInternodes task
-        process = run_task(real_plant_data, "AnglesAndInternodes", geom_pipe_real_conf, no_auth=True)
+        process = run_task(plant_dataset, "AnglesAndInternodes", geom_pipe_real_conf, no_auth=True)
         self.assertTrue(process.returncode == 0)
 
         # Check if a minimum number of angles and internodes were computed
-        with open(glob.glob(real_plant_data + "AnglesAndInternodes_*" + "/" + "AnglesAndInternodes.json")[0]) as f:
+        with open(glob.glob(plant_dataset + "AnglesAndInternodes_*" + "/" + "AnglesAndInternodes.json")[0]) as f:
             json_data = json.load(f)
 
         angles = json_data["angles"]
@@ -43,23 +43,25 @@ class TestGeomAnglesAndInternodes(unittest.TestCase):
     def test_real_plant_custom_matcher(self):
         geom_pipe_real_conf = os.path.join(Path(__file__).parents[2], "configs/test_geom_pipe_real.toml")
         print(f"Testing geometric pipeline with conf: {geom_pipe_real_conf}")
-        real_plant_data = os.path.join(Path(__file__).parents[1], "testdata/real_plant/")
-        print(f"Testing geometric pipeline with data: {real_plant_data}")
+        plant_dataset = os.path.join(Path(__file__).parents[1], "testdata/real_plant/")
+        print(f"Testing geometric pipeline with data: {plant_dataset}")
 
+        print("Modifying the configuration to use the custom 'Colmap.matcher'...")
+        # Load the TOML config for the reconstruction pipeline and change the 'Colmap.matcher' to "custom":
         with open(geom_pipe_real_conf, 'r') as f:
             custom_config = toml.load(f)
         custom_config["Colmap"]["matcher"] = "custom"
 
-        # Perform a Clean before running the pipe
-        process = run_task(real_plant_data, "Clean", geom_pipe_real_conf, no_auth=True)
+        # Perform a Clean before running the pipeline
+        process = run_task(plant_dataset, "Clean", geom_pipe_real_conf, no_auth=True)
         self.assertTrue(process.returncode == 0)
 
         # Perform the AnglesAndInternodes task
-        process = run_task(real_plant_data, "AnglesAndInternodes", custom_config, no_auth=True)
+        process = run_task(plant_dataset, "AnglesAndInternodes", custom_config, no_auth=True)
         self.assertTrue(process.returncode == 0)
 
         # Check if a minimum number of angles and internodes were computed
-        with open(glob.glob(real_plant_data + "AnglesAndInternodes_*" + "/" + "AnglesAndInternodes.json")[0]) as f:
+        with open(glob.glob(plant_dataset + "AnglesAndInternodes_*" + "/" + "AnglesAndInternodes.json")[0]) as f:
             json_data = json.load(f)
 
         angles = json_data["angles"]
@@ -106,25 +108,25 @@ class TestMLAnglesAndInternodes(unittest.TestCase):
     def test_real_plant(self):
         ml_pipe_real_conf = os.path.join(Path(__file__).parents[2], "configs/test_ml_pipe_real.toml")
         print(f"Testing CNN pipeline with conf: {ml_pipe_real_conf}")
-        real_plant_data = os.path.join(Path(__file__).parents[1], "testdata/real_plant/")
-        print(f"Testing CNN pipeline with data: {real_plant_data}")
+        plant_dataset = os.path.join(Path(__file__).parents[1], "testdata/real_plant/")
+        print(f"Testing CNN pipeline with data: {plant_dataset}")
 
         # Perform a Clean before running the pipe
-        process = run_task(real_plant_data, "Clean", ml_pipe_real_conf, no_auth=True)
+        process = run_task(plant_dataset, "Clean", ml_pipe_real_conf, no_auth=True)
         self.assertTrue(process.returncode == 0)
 
         # Make sure that the weights files exists, otherwise download it
         fname = "Resnet_896_896_epoch50.pt"
         model_name = os.path.join(Path(__file__).parents[1], "testdata/models/models/" + fname)
         if not os.path.exists(model_name):
-            get_models_dataset(Path(real_plant_data).parent)
+            get_models_dataset(Path(plant_dataset).parent)
 
         # Perform the AnglesAndInternodes
-        process = run_task(real_plant_data, "AnglesAndInternodes", ml_pipe_real_conf, no_auth=True)
+        process = run_task(plant_dataset, "AnglesAndInternodes", ml_pipe_real_conf, no_auth=True)
         self.assertTrue(process.returncode == 0)
 
         # Check if a minimum number of angles and internodes were computed
-        with open(glob.glob(real_plant_data + "AnglesAndInternodes_*" + "/" + "AnglesAndInternodes.json")[0]) as f:
+        with open(glob.glob(plant_dataset + "AnglesAndInternodes_*" + "/" + "AnglesAndInternodes.json")[0]) as f:
             json_data = json.load(f)
 
         angles = json_data["angles"]
